02e343dcc5d5ca8378018ec181dcb3af30723313
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-27 10:23:38 +0100

    .then calls on seperate lines. Adjusted spacing.

--- app/process-tasks.js
+++ app/process-tasks.js
@@ -9,31 +9,33 @@ const getWorkerForTask = require('./services/get-worker-for-task')
 module.exports = function () {
   var batchSize = config.ASYNC_WORKER_BATCH_SIZE
 
-  return getPendingTasksAndMarkInProgress(batchSize).then(function (tasks) {
-    log.info(`found ${tasks.length} tasks`)
-    if (tasks.length === 0) { return }
+  return getPendingTasksAndMarkInProgress(batchSize)
+    .then(function (tasks) {
+      log.info(`found ${tasks.length} tasks`)
+      if (tasks.length === 0) { return }
 
-    var promiseArray = []
+      var promiseArray = []
 
-    for (var task of tasks) {
-      var worker = getWorkerForTask(task.task)
+      for (var task of tasks) {
+        var worker = getWorkerForTask(task.task)
 
-      if (worker) {
-        log.info(`started task: ${task.taskId}-${task.task}`)
+        if (worker) {
+          log.info(`started task: ${task.taskId}-${task.task}`)
 
-        promiseArray.push(worker.execute(task).then(function () {
-          return completeTaskWithStatus(task.taskId, statusEnum.COMPLETE)
-        }).catch(function (error) {
-          log.info(`error running task: ${task.taskId}-${task.task}, error: ${error}`)
-          return completeTaskWithStatus(task.taskId, statusEnum.FAILED)
-        }))
-      } else {
-        log.info(`unable to find worker for task: ${task.task}`)
+          promiseArray.push(worker.execute(task)
+            .then(function () {
+              return completeTaskWithStatus(task.taskId, statusEnum.COMPLETE)
+            }).catch(function (error) {
+              log.info(`error running task: ${task.taskId}-${task.task}, error: ${error}`)
+              return completeTaskWithStatus(task.taskId, statusEnum.FAILED)
+            }))
+        } else {
+          log.info(`unable to find worker for task: ${task.task}`)
+        }
       }
-    }
 
-    return Promise.each(promiseArray, function (result) {
-      log.info(`completed task: ${task.taskId}-${task.task}`)
+      return Promise.each(promiseArray, function (result) {
+        log.info(`completed task: ${task.taskId}-${task.task}`)
+      })
     })
-  })
 }
