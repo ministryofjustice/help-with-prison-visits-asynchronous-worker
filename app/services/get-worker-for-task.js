bfc7eac790ae7eb5d1173268b8fb7b6c68b50015
Steven Alexander <steven.william.alexander@googlemail.com>
2016-10-31 10:43:20 +0000

    apvs-359 - add to get worker for task

--- app/services/get-worker-for-task.js
+++ app/services/get-worker-for-task.js
@@ -1,9 +1,15 @@
 var tasksEnum = require('../constants/tasks-enum')
+var sendAcceptedClaimNotification = require('./workers/send-accepted-claim-notification')
+var sendRejectedClaimNotification = require('./workers/send-rejected-claim-notification')
 var sendFirstTimeClaimNotification = require('./workers/send-first-time-claim-notification')
 
 // ALL WORKERS SHOULD HAVE A METHOD `execute(task)` that returns a Promise
 module.exports = function (taskType) {
-  if (taskType === tasksEnum.FIRST_TIME_CLAIM_NOTIFICATION) {
+  if (taskType === tasksEnum.ACCEPT_CLAIM_NOTIFICATION) {
+    return sendAcceptedClaimNotification
+  } else if (taskType === tasksEnum.REJECT_CLAIM_NOTIFICATION) {
+    return sendRejectedClaimNotification
+  } else if (taskType === tasksEnum.FIRST_TIME_CLAIM_NOTIFICATION) {
     return sendFirstTimeClaimNotification
   }
   return null
--- test/unit/services/test-get-worker-for-task.js
+++ test/unit/services/test-get-worker-for-task.js
@@ -2,12 +2,27 @@ const expect = require('chai').expect
 const proxyquire = require('proxyquire')
 const tasksEnum = require('../../../app/constants/tasks-enum')
 
+const sendAcceptedClaimNotification = { name: 'sendAcceptedClaimNotification' }
+const sendRejectedClaimNotification = { name: 'sendRejectedClaimNotification' }
 const sendFirstTimeClaimNotification = { name: 'sendFirstTimeClaimNotification' }
 const getWorkerForTask = proxyquire('../../../app/services/get-worker-for-task', {
+  './workers/send-accepted-claim-notification': sendAcceptedClaimNotification,
+  './workers/send-rejected-claim-notification': sendRejectedClaimNotification,
   './workers/send-first-time-claim-notification': sendFirstTimeClaimNotification
 })
 
 describe('services/getWorkerForTask', function () {
+  it('should return send-accepted-claim-notification', function (done) {
+    var worker = getWorkerForTask(tasksEnum.ACCEPT_CLAIM_NOTIFICATION)
+    expect(worker.name).to.be.equal('sendAcceptedClaimNotification')
+    done()
+  })
+
+  it('should return send-rejected-claim-notification', function (done) {
+    var worker = getWorkerForTask(tasksEnum.REJECT_CLAIM_NOTIFICATION)
+    expect(worker.name).to.be.equal('sendRejectedClaimNotification')
+    done()
+  })
   it('should return send-first-time-claim-notification', function (done) {
     var worker = getWorkerForTask(tasksEnum.FIRST_TIME_CLAIM_NOTIFICATION)
     expect(worker.name).to.be.equal('sendFirstTimeClaimNotification')
