116cdfa39dc99597fab80e831611622dbe8687bc
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-07 12:07:37 +0000

    chore - add test email which will not send a notification

--- test/integration/services/notify/test-send-notification.js
+++ test/integration/services/notify/test-send-notification.js
@@ -2,14 +2,19 @@ const config = require('../../../../config')
 const sendNotification = require('../../../../app/services/notify/send-notification')
 
 describe('services/notify/send-notification', function () {
-  it('should send an email', function (done) {
-    sendNotification(
+  it('should send an email', function () {
+    return sendNotification(
       config.NOTIFY_FIRST_TIME_CLAIM_EMAIL_TEMPLATE_ID,
       process.env.APVS_TEST_EMAIL_ADDRESS,
       { reference: '1234567' }
-    ).then(function () {
-      // manual check to see if email was sent
-      done()
-    })
+    ) // manual check to see if email was sent
+  })
+
+  it('should not send an email to do not send email', function () {
+    return sendNotification(
+      config.NOTIFY_FIRST_TIME_CLAIM_EMAIL_TEMPLATE_ID,
+      config.NOTIFY_DO_NOT_SEND_EMAIL,
+      { reference: 'DONOTSEND' }
+    ) // manual check to see if email was not sent
   })
 })
