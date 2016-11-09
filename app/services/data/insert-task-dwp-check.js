2b682f848221d1691bf49c04297ab2ea2f24ada2
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-09 16:57:24 +0000

    insert DWP check after submitting claim

---
+++ app/services/data/insert-task-dwp-check.js
@@ -0,0 +1,16 @@
+const config = require('../../../knexfile').asyncworker
+const knex = require('knex')(config)
+const tasksEnum = require('../../constants/tasks-enum')
+const statusEnum = require('../../constants/status-enum')
+
+module.exports = function (reference, claimId) {
+  var task = {
+    'Task': tasksEnum.DWP_CHECK,
+    'Reference': reference,
+    'ClaimId': claimId,
+    'DateCreated': new Date(),
+    'Status': statusEnum.INPROGRESS
+  }
+
+  return knex('IntSchema.Task').insert(task).returning('TaskId')
+}
--- app/services/workers/complete-first-time-claim.js
+++ app/services/workers/complete-first-time-claim.js
@@ -3,6 +3,7 @@ const statusEnum = require('../../constants/status-enum')
 const getAllFirstTimeClaimData = require('../data/get-all-first-time-claim-data')
 const copyFirstTimeClaimDataToInternal = require('../data/copy-first-time-claim-data-to-internal')
 const deleteFirstTimeClaimFromExternal = require('../data/delete-first-time-claim-from-external')
+const insertTaskDwpCheck = require('../data/insert-task-dwp-check')
 
 module.exports.execute = function (task) {
   var reference = task.reference
@@ -11,4 +12,5 @@ module.exports.execute = function (task) {
   return getAllFirstTimeClaimData(reference, claimId, statusEnum.SUBMITTED)
           .then(function (firstTimeClaimData) { return copyFirstTimeClaimDataToInternal(firstTimeClaimData) })
           .then(function () { return deleteFirstTimeClaimFromExternal(reference, claimId) })
+          .then(function () { return insertTaskDwpCheck(reference, claimId) })
 }
