0883190aa8e3821d52adb79698156b481b5c9b07
Gary Douglas <garyd@kainos.com>
2016-11-16 12:47:23 +0000

    Fixed merge issue in test-send-notification

--- test/integration/services/notify/test-send-notification.js
+++ test/integration/services/notify/test-send-notification.js
@@ -5,7 +5,6 @@ const sendNotification = require('../../../../app/services/notify/send-notificat
 describe('services/notify/send-notification', function () {
   it('should send an email', function () {
     return sendNotification(
-      config.NOTIFY_FIRST_TIME_CLAIM_EMAIL_TEMPLATE_ID,
       config.NOTIFY_ACCEPTED_CLAIM_EMAIL_TEMPLATE_ID,
       config.APVS_TEST_EMAIL_ADDRESS,
       { reference: '1234567',
@@ -20,8 +19,6 @@ describe('services/notify/send-notification', function () {
   it('should not send an email to do not send email', function () {
     return sendNotification(
       config.NOTIFY_FIRST_TIME_CLAIM_EMAIL_TEMPLATE_ID,
-      // config.NOTIFY_FIRST_TIME_CLAIM_EMAIL_TEMPLATE_ID,
-      config.APVS_NOTIFY_ACCEPTED_CLAIM_EMAIL_TEMPLATE_ID,
       config.NOTIFY_DO_NOT_SEND_EMAIL,
       { reference: 'DONOTSEND' }
     ) // manual check to see if email was not sent
