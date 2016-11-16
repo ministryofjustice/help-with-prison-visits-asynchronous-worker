418e4721aaac9b9e8511f935bab4082bdaa0aa3a
Gary Douglas <garyd@kainos.com>
2016-11-16 16:54:02 +0000

    Removed database calls from unit test

--- test/unit/services/notify/helpers/test-get-approved-claim-details-string.js
+++ test/unit/services/notify/helpers/test-get-approved-claim-details-string.js
@@ -1,92 +1,68 @@
 const expect = require('chai').expect
-const config = require('../../../../../knexfile').asyncworker
-const knex = require('knex')(config)
-const testHelper = require('../../../../test-helper')
 const getApprovedClaimDetailsString = require('../../../../../app/services/notify/helpers/get-approved-claim-details-string')
 
 describe('notify/helpers/get-approved-claim-details-string', function () {
-  var reference = 'DWPVISI'
-  var claimId
-  var claimExpenseId1
-  var claimExpenseId2
-
-  beforeEach(function () {
-    return testHelper.insertClaimEligibilityData('IntSchema', reference)
-      .then(function (ids) {
-        claimId = ids.claimId
-      })
-      .then(function () {
-        return knex('IntSchema.ClaimExpense')
-          .where('ClaimId', claimId)
-          .select('ClaimExpenseId')
-      })
-      .then(function (claimExpenses) {
-        claimExpenseId1 = claimExpenses[0].ClaimExpenseId
-        claimExpenseId2 = claimExpenses[1].ClaimExpenseId
-
-        // Set one expense to REJECTED
-        return knex('IntSchema.ClaimExpense')
-          .where('ClaimExpenseId', claimExpenseId1)
-          .update({
-            'Status': 'REJECTED',
-            'ExpenseType': 'accommodation',
-            'ApprovedCost': 0
-          })
-      })
-      // Set one expense to APPROVED
-      .then(function () {
-        return knex('IntSchema.ClaimExpense')
-          .where('ClaimExpenseId', claimExpenseId2)
-          .update({
-            'Status': 'APPROVED',
-            'ApprovedCost': 20,
-            'Cost': 30
-          })
-      })
-  })
+  var claimExpense1 = [{
+    ClaimExpenseId: 793150379,
+    EligibilityId: 793150379,
+    Reference: 'DWPVISI',
+    ClaimId: 793150379,
+    ExpenseType: 'accommodation',
+    Cost: 0,
+    TravelTime: null,
+    From: 'London',
+    To: 'Hewell',
+    IsReturn: false,
+    DurationOfTravel: null,
+    TicketType: null,
+    IsChild: null,
+    IsEnabled: true,
+    ApprovedCost: 0,
+    Note: null,
+    Status: 'REJECTED'
+  }]
+  var claimExpense2 = [{
+    ClaimExpenseId: 793149603,
+    EligibilityId: 793149602,
+    Reference: 'DWPVISI',
+    ClaimId: 793149602,
+    ExpenseType: 'bus',
+    Cost: 30,
+    TravelTime: null,
+    From: 'Euston',
+    To: 'Birmingham New Street',
+    IsReturn: false,
+    DurationOfTravel: null,
+    TicketType: null,
+    IsChild: null,
+    IsEnabled: true,
+    ApprovedCost: 20,
+    Note: null,
+    Status: 'APPROVED'
+  }]
 
   it('should contain journey info if claim expense type is a journey type', function () {
-    return knex('IntSchema.ClaimExpense')
-      .where('ClaimExpenseId', claimExpenseId2)
-      .then(function (claimExpense) {
-        var claimDetails = getApprovedClaimDetailsString(claimExpense)
-        expect(claimDetails).to.contain('Bus journey - Euston to Birmingham')
-      })
+    var claimDetails = getApprovedClaimDetailsString(claimExpense2)
+    expect(claimDetails).to.contain('Bus journey - Euston to Birmingham')
   })
 
   it('should contain just the expense type if claim expense type is not a journey type', function () {
-    return knex('IntSchema.ClaimExpense')
-      .where('ClaimExpenseId', claimExpenseId1)
-      .then(function (claimExpense) {
-        var claimDetails = getApprovedClaimDetailsString(claimExpense)
-        expect(claimDetails).to.contain('Accommodation')
-      })
+    var claimDetails = getApprovedClaimDetailsString(claimExpense1)
+    expect(claimDetails).to.contain('Accommodation')
   })
 
   it('should contain the correct claim amount', function () {
-    return knex('IntSchema.ClaimExpense')
-      .where('ClaimExpenseId', claimExpenseId2)
-      .then(function (claimExpense) {
-        var claimDetails = getApprovedClaimDetailsString(claimExpense)
-        expect(claimDetails).to.contain('Claimed: £30.00')
-      })
+    var claimDetails = getApprovedClaimDetailsString(claimExpense2)
+    expect(claimDetails).to.contain('Claimed: £30.00')
   })
 
   it('should contain the correct approved amount', function () {
-    return knex('IntSchema.ClaimExpense')
-      .where('ClaimExpenseId', claimExpenseId2)
-      .then(function (claimExpense) {
-        var claimDetails = getApprovedClaimDetailsString(claimExpense)
-        expect(claimDetails).to.contain('Approved: £20.00')
-      })
+    var claimDetails = getApprovedClaimDetailsString(claimExpense2)
+    expect(claimDetails).to.contain('Approved: £20.00')
   })
 
   it('should return nothing if no claims expenses exist', function () {
-    return knex('IntSchema.ClaimExpense')
-      .where('ClaimExpenseId', '0')
-      .then(function (claimExpense) {
-        var claimDetails = getApprovedClaimDetailsString(claimExpense)
-        expect(claimDetails).to.equal('')
-      })
+    var claimDetails = getApprovedClaimDetailsString([])
+    expect(claimDetails).to.equal('')
   })
 })
