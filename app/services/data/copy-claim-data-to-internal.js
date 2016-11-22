e9af27a5b631ae3fed0538ffce2c5f05f099d463
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-22 16:45:56 +0000

    updated copy to copy with repeat claim

--- app/services/data/copy-claim-data-to-internal.js
+++ app/services/data/copy-claim-data-to-internal.js
@@ -3,18 +3,35 @@ const knex = require('knex')(config)
 const statusEnum = require('../../constants/status-enum')
 
 module.exports = function (data) {
-  data.Eligibility.Status = statusEnum.NEW
+  return copyEligibilityDataIfPresent(data)
+    .then(function () { return copyClaimData(data) })
+}
 
-  return knex('IntSchema.Eligibility').insert(data.Eligibility)
-    .then(function () {
-      data.Claim.Status = statusEnum.NEW
-      data.ClaimDocument.forEach(function (document) {
-        if (document.DocumentStatus !== 'uploaded') {
-          data.Claim.Status = statusEnum.PENDING
-        }
+function copyEligibilityDataIfPresent (data) {
+  if (data.Eligibility) {
+    data.Eligibility.Status = statusEnum.NEW
+
+    return knex('IntSchema.Eligibility').insert(data.Eligibility)
+      .then(function () {
+        return knex('IntSchema.Visitor').insert(data.Visitor)
       })
-      return knex('IntSchema.Claim').insert(data.Claim)
-    })
+      .then(function () {
+        return knex('IntSchema.Prisoner').insert(data.Prisoner)
+      })
+  } else {
+    return Promise.resolve()
+  }
+}
+
+function copyClaimData (data) {
+  data.Claim.Status = statusEnum.NEW
+  data.ClaimDocument.forEach(function (document) {
+    if (document.DocumentStatus !== 'uploaded') {
+      data.Claim.Status = statusEnum.PENDING
+    }
+  })
+
+  return knex('IntSchema.Claim').insert(data.Claim)
     .then(function () {
       return knex('IntSchema.ClaimBankDetail').insert(data.ClaimBankDetail)
     })
@@ -29,11 +46,5 @@ module.exports = function (data) {
     })
     .then(function () {
       return knex('IntSchema.ClaimDocument').insert(data.ClaimDocument)
-    })
-    .then(function () {
-      return knex('IntSchema.Visitor').insert(data.Visitor)
-    })
-    .then(function () {
-      return knex('IntSchema.Prisoner').insert(data.Prisoner)
     })
 }
--- test/integration/services/data/test-copy-claim-data-to-internal.js
+++ test/integration/services/data/test-copy-claim-data-to-internal.js
@@ -5,54 +5,72 @@ const statusEnum = require('../../../../app/constants/status-enum')
 const testHelper = require('../../../test-helper')
 
 const copyClaimDataToInternal = require('../../../../app/services/data/copy-claim-data-to-internal')
-
-var reference = 'COPY123'
-var claimId = 123
-var firstTimeClaimData = testHelper.getFirstTimeClaimData(reference, claimId)
-
+const reference = 'COPY123'
+const claimId = 123
 describe('services/data/copy-first-time-claim-data-to-internal', function () {
-  it('should copy the first time claim data to internal', function () {
-    return copyClaimDataToInternal(firstTimeClaimData).then(function () {
-      return knex('IntSchema.Eligibility').where('IntSchema.Eligibility.Reference', reference)
-        .join('IntSchema.Prisoner', 'IntSchema.Eligibility.EligibilityId', '=', 'IntSchema.Prisoner.EligibilityId')
-        .join('IntSchema.Visitor', 'IntSchema.Eligibility.EligibilityId', '=', 'IntSchema.Visitor.EligibilityId')
-        .join('IntSchema.Claim', 'IntSchema.Eligibility.EligibilityId', '=', 'IntSchema.Claim.EligibilityId')
-        .join('IntSchema.ClaimBankDetail', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimBankDetail.ClaimId')
-        .join('IntSchema.ClaimExpense', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimExpense.ClaimId')
-        .select()
-        .then(function (results) {
-          expect(results[0].Status[0], 'Eligibility.Status should be NEW').to.be.equal(statusEnum.NEW)
-          expect(results[0].Status[1], 'Claim.Status should be NEW').to.be.equal(statusEnum.NEW)
-          expect(results[0].AccountNumber).to.be.equal(firstTimeClaimData.ClaimBankDetail.AccountNumber)
-          expect(results.length, 'Should have two ClaimExpense').to.be.equal(2)
-          expect(results[0].ExpenseType).to.be.equal('car')
-          expect(results[1].Cost).to.be.equal(20.95)
-          expect(results[0].NationalInsuranceNumber).to.be.equal(firstTimeClaimData.Visitor.NationalInsuranceNumber)
-          expect(results[0].PrisonNumber).to.be.equal(firstTimeClaimData.Prisoner.PrisonNumber)
-        })
+  describe('first time claim', function () {
+    var firstTimeClaimData = testHelper.getFirstTimeClaimData(reference, claimId)
+
+    it('should copy the first time claim data to internal and set status to new', function () {
+      return copyClaimDataToInternal(firstTimeClaimData).then(function () {
+        return knex('IntSchema.Eligibility').where('IntSchema.Eligibility.Reference', reference)
+          .join('IntSchema.Prisoner', 'IntSchema.Eligibility.EligibilityId', '=', 'IntSchema.Prisoner.EligibilityId')
+          .join('IntSchema.Visitor', 'IntSchema.Eligibility.EligibilityId', '=', 'IntSchema.Visitor.EligibilityId')
+          .join('IntSchema.Claim', 'IntSchema.Eligibility.EligibilityId', '=', 'IntSchema.Claim.EligibilityId')
+          .join('IntSchema.ClaimChild', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimChild.ClaimId')
+          .join('IntSchema.ClaimBankDetail', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimBankDetail.ClaimId')
+          .join('IntSchema.ClaimExpense', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimExpense.ClaimId')
+          .select()
+          .then(function (results) {
+            expect(results[0].Status[0], 'Eligibility.Status should be NEW').to.be.equal(statusEnum.NEW)
+            expect(results[0].Status[1], 'Claim.Status should be NEW').to.be.equal(statusEnum.NEW)
+            expect(results[0].AccountNumber).to.be.equal(firstTimeClaimData.ClaimBankDetail.AccountNumber)
+            expect(results.length, 'Should have four rows, two child and two expense').to.be.equal(4)
+            expect(results[0].ExpenseType).to.be.equal('car')
+            expect(results[2].Cost).to.be.equal(20.95)
+            expect(results[0].NationalInsuranceNumber).to.be.equal(firstTimeClaimData.Visitor.NationalInsuranceNumber)
+            expect(results[0].PrisonNumber).to.be.equal(firstTimeClaimData.Prisoner.PrisonNumber)
+          })
+      })
+    })
+
+    it('should change claim status to PENDING if documents not uploaded', function () {
+      firstTimeClaimData.ClaimDocument[0].DocumentStatus = 'post-later'
+      firstTimeClaimData.ClaimDocument[1].DocumentStatus = 'upload-later'
+
+      return copyClaimDataToInternal(firstTimeClaimData).then(function () {
+        return knex('IntSchema.Claim').where('IntSchema.Claim.Reference', reference)
+          .select('Claim.Status')
+          .then(function (results) {
+            expect(results[0].Status, 'Claim.Status should be PENDING').to.be.equal(statusEnum.PENDING)
+          })
+      })
     })
   })
 
-  after(function () {
-    return testHelper.deleteAll(reference, 'IntSchema')
-  })
-})
+  describe('repeat claim', function () {
+    const repeatClaimData = testHelper.getFirstTimeClaimData(reference, claimId)
+    repeatClaimData.Eligibility = undefined
+    repeatClaimData.Visitor = undefined
+    repeatClaimData.Prisoner = undefined
 
-describe('services/data/copy-first-time-claim-data-to-internal', function () {
-  it('should change claim status to PENDING if documents not uploaded', function () {
-    firstTimeClaimData.ClaimDocument[0].DocumentStatus = 'post-later'
-    firstTimeClaimData.ClaimDocument[1].DocumentStatus = 'upload-later'
-
-    return copyClaimDataToInternal(firstTimeClaimData).then(function () {
-      return knex('IntSchema.Claim').where('IntSchema.Claim.Reference', reference)
-        .select('Claim.Status')
-        .then(function (results) {
-          expect(results[0].Status, 'Claim.Status should be PENDING').to.be.equal(statusEnum.PENDING)
-        })
+    it('should change claim status to PENDING if documents not uploaded', function () {
+      return copyClaimDataToInternal(repeatClaimData).then(function () {
+        return knex('IntSchema.Claim').where('IntSchema.Claim.Reference', reference)
+          .join('IntSchema.ClaimChild', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimChild.ClaimId')
+          .join('IntSchema.ClaimBankDetail', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimBankDetail.ClaimId')
+          .join('IntSchema.ClaimExpense', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimExpense.ClaimId')
+          .select()
+          .then(function (results) {
+            expect(results.length, 'Should have four rows, two child and two expense').to.be.equal(4)
+            expect(results[0].Status[0], 'Claim.Status should be NEW').to.be.equal(statusEnum.NEW)
+            expect(results[0].Reference[0]).to.be.equal(reference)
+          })
+      })
     })
   })
 
-  after(function () {
+  afterEach(function () {
     return testHelper.deleteAll(reference, 'IntSchema')
   })
 })
