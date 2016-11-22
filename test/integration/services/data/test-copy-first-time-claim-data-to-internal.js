163ff312335a1b2f43fad627d0f2d2b5fac99e13
Paul Wright <wrightpaul08@gmail.com>
2016-11-22 15:49:27 +0000

    feature/apvs-372/mark upload later claims as pending (#24)
    * apvs-372 - mark upload later claims pending
    
    * refactor integration test
    
    * refactor integration test

--- test/integration/services/data/test-copy-first-time-claim-data-to-internal.js
+++ test/integration/services/data/test-copy-first-time-claim-data-to-internal.js
@@ -5,12 +5,11 @@ const statusEnum = require('../../../../app/constants/status-enum')
 const testHelper = require('../../../test-helper')
 
 const copyFirstTimeClaimDataToInternal = require('../../../../app/services/data/copy-first-time-claim-data-to-internal')
+var reference = 'COPY123'
+var claimId = 123
+var firstTimeClaimData = testHelper.getFirstTimeClaimData(reference, claimId)
 
 describe('services/data/copy-first-time-claim-data-to-internal', function () {
-  var reference = 'COPY123'
-  var claimId = 123
-  var firstTimeClaimData = testHelper.getFirstTimeClaimData(reference, claimId)
-
   it('should copy the first time claim data to internal', function () {
     return copyFirstTimeClaimDataToInternal(firstTimeClaimData).then(function () {
       return knex('IntSchema.Eligibility').where('IntSchema.Eligibility.Reference', reference)
@@ -22,7 +21,7 @@ describe('services/data/copy-first-time-claim-data-to-internal', function () {
         .select()
         .then(function (results) {
           expect(results[0].Status[0], 'Eligibility.Status should be NEW').to.be.equal(statusEnum.NEW)
-          expect(results[0].Status[1], 'Claim.Status should be PENDING').to.be.equal(statusEnum.PENDING)
+          expect(results[0].Status[1], 'Claim.Status should be NEW').to.be.equal(statusEnum.NEW)
           expect(results[0].AccountNumber).to.be.equal(firstTimeClaimData.ClaimBankDetail.AccountNumber)
           expect(results.length, 'Should have two ClaimExpense').to.be.equal(2)
           expect(results[0].ExpenseType).to.be.equal('car')
@@ -31,22 +30,23 @@ describe('services/data/copy-first-time-claim-data-to-internal', function () {
           expect(results[0].PrisonNumber).to.be.equal(firstTimeClaimData.Prisoner.PrisonNumber)
         })
     })
-    .then(function () {
-      return testHelper.deleteAll(reference, 'IntSchema')
-    })
   })
 
-  it('should copy the first time claim data to internal with claim status NEW if all documents uploaded', function () {
-    firstTimeClaimData.ClaimDocument.forEach(function (document) {
-      if (document.DocumentStatus !== 'uploaded') {
-        document.DocumentStatus = 'uploaded'
-      }
-    })
+  after(function () {
+    return testHelper.deleteAll(reference, 'IntSchema')
+  })
+})
+
+describe('services/data/copy-first-time-claim-data-to-internal', function () {
+  it('should change claim status to PENDING if documents not uploaded', function () {
+    firstTimeClaimData.ClaimDocument[0].DocumentStatus = 'post-later'
+    firstTimeClaimData.ClaimDocument[1].DocumentStatus = 'upload-later'
+
     return copyFirstTimeClaimDataToInternal(firstTimeClaimData).then(function () {
       return knex('IntSchema.Claim').where('IntSchema.Claim.Reference', reference)
         .select('Claim.Status')
         .then(function (results) {
-          expect(results[0].Status, 'Claim.Status should be NEW').to.be.equal(statusEnum.NEW)
+          expect(results[0].Status, 'Claim.Status should be PENDING').to.be.equal(statusEnum.PENDING)
         })
     })
   })
