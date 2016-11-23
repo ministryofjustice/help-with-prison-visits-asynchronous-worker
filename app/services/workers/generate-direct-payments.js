eff67146f785369334cd22dcf74b1107f138c62e
Thomas Swann <ts.swann@gmail.com>
2016-11-23 19:07:02 +0000

    Adding stubs for CSV generation and worker to call method to get data

--- app/services/workers/generate-direct-payments.js
+++ app/services/workers/generate-direct-payments.js
@@ -1,3 +1,9 @@
+const getClaimsPendingPayment = require('../data/get-claims-pending-payment')
+const logger = require('../log')
 
 module.exports.execute = function (task) {
+  return getClaimsPendingPayment()
+    .then(function (paymentData) {
+      logger.info(paymentData)
+    })
 }
