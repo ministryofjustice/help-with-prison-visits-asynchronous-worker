d319df6a5a4331376aced74d629fcb55cebbc66a
Thomas Swann <ts.swann@gmail.com>
2016-11-25 10:37:21 +0000

    Fixed standard indentation errors

--- app/services/data/get-claims-pending-payment.js
+++ app/services/data/get-claims-pending-payment.js
@@ -4,7 +4,7 @@ const _ = require('lodash')
 const claimStatuses = require('../../constants/claim-status-enum')
 
 const selectColumns = ['IntSchema.ClaimBankDetail.SortCode', 'IntSchema.ClaimBankDetail.AccountNumber',
-      'IntSchema.Visitor.FirstName', 'IntSchema.Visitor.LastName', 'IntSchema.Claim.Reference']
+  'IntSchema.Visitor.FirstName', 'IntSchema.Visitor.LastName', 'IntSchema.Claim.Reference']
 
 module.exports = function () {
   return knex('IntSchema.Claim')
@@ -14,7 +14,7 @@ module.exports = function () {
     .innerJoin('IntSchema.Visitor', 'IntSchema.Claim.EligibilityId', '=', 'IntSchema.Visitor.EligibilityId')
     .innerJoin('IntSchema.ClaimExpense', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.ClaimExpense.ClaimId')
     .where({'IntSchema.Claim.Status': claimStatuses.APPROVED,
-            'IntSchema.ClaimExpense.Status': claimStatuses.APPROVED})
+      'IntSchema.ClaimExpense.Status': claimStatuses.APPROVED})
     .whereNull('IntSchema.Claim.PaymentStatus')
     .groupBy(selectColumns)
     .then(function (results) {
