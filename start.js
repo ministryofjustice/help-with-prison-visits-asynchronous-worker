1bfe71a3f3f2a8440e87abe64944ed5492c7ca94
Thomas Swann <ts.swann@gmail.com>
2016-11-29 08:04:35 +0000

    Removing redundant job code for testing

--- start.js
+++ start.js
@@ -2,13 +2,10 @@ const config = require('./config')
 const log = require('./app/services/log')
 var throng = require('throng')
 var processTasks = require('./app/process-tasks')
-var scheduleGenerateDirectPayments = require('./app/services/data/insert-task-generate-direct-payments')
 
 var numberOfWorkers = config.ASYNC_WORKER_CONCURRENCY
 var frequency = config.ASYNC_WORKER_FREQUENCY
 var startWeb = config.ASYNC_START_WEB === 'true'
-var runJobs = 'true'
-
 throng({ workers: numberOfWorkers }, startWorker)
 
 function startWorker (id) {
@@ -18,14 +15,6 @@ function startWorker (id) {
     log.info(`Worker ${id} exiting...`)
     process.exit()
   })
-
-  // Testing - TODO: Schedule on Cron
-  if (runJobs) {
-    runScheduledJobs()
-      .then(function () {
-        runJobs = false
-      })
-  }
 
   runProcessTasks(id).then(function () {
     setInterval(function () {
@@ -37,10 +26,6 @@ function startWorker (id) {
     // Start web
     require('./app/web/bin/www')
   }
-}
-
-function runScheduledJobs (id) {
-  return scheduleGenerateDirectPayments()
 }
 
 function runProcessTasks (id) {
