deb937c31169c2e7a2aafcf168152f01e578885e
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-30 16:20:02 +0000

    removed multiple claim test data and added new checks

--- test/integration/services/data/test-auto-approve-claim.js
+++ test/integration/services/data/test-auto-approve-claim.js
@@ -7,40 +7,45 @@ require('sinon-bluebird')
 
 const testHelper = require('../../../test-helper')
 const statusEnum = require('../../../../app/constants/status-enum')
+const tasksEnum = require('../../../../app/constants/tasks-enum')
+
 const autoApproveClaimExpenseStub = sinon.stub().resolves()
+const insertTaskStub = sinon.stub().resolves()
+
 const autoApproveClaim = proxyquire('../../../../app/services/data/auto-approve-claim', {
-  './auto-approve-claim-expenses': autoApproveClaimExpenseStub
+  './auto-approve-claim-expenses': autoApproveClaimExpenseStub,
+  '../data/insert-task': insertTaskStub
 })
 
-const reference = 'AUTOAPP'
+const REFERENCE = 'AUTOAPP'
+const EMAIL_ADDRESS = 'donotsend@apvs.com'
+var eligibilityId
 var claimId
 
 describe('services/data/auto-approve-claim', function () {
   before(function () {
-    return testHelper.insertClaimEligibilityData('IntSchema', reference)
+    return testHelper.insertClaimEligibilityData('IntSchema', REFERENCE)
       .then(function (ids) {
+        eligibilityId = ids.eligibilityId
         claimId = ids.claimId
       })
   })
 
-  it('should update the status of the claim to AUTOAPPROVED', function () {
-    var claimData = testHelper.getClaimData(reference)
-    return testHelper.insertClaimData('IntSchema', reference, claimData.Claim.EligibilityId, claimData)
-      .then(function (id) {
-        claimId = id
-        return autoApproveClaim(claimData.Claim.ClaimId, 'donotsend@apvs.com')
-          .then(function () {
-            return knex('IntSchema.Claim')
-              .where('ClaimId', claimId)
-              .first()
-              .then(function (claim) {
-                expect(claim.Status).to.equal(statusEnum.AUTOAPPROVED)
-              })
+  it('should update the status of the claim to AUTOAPPROVED, call to update expenses and send accept email', function () {
+    return autoApproveClaim(claimId, EMAIL_ADDRESS)
+      .then(function () {
+        return knex('IntSchema.Claim')
+          .where('ClaimId', claimId)
+          .first()
+          .then(function (claim) {
+            expect(claim.Status).to.equal(statusEnum.AUTOAPPROVED)
+            expect(autoApproveClaimExpenseStub.calledWith(claimId)).to.be.true
+            expect(insertTaskStub.calledWith(REFERENCE, eligibilityId, claimId, tasksEnum.ACCEPT_CLAIM_NOTIFICATION, EMAIL_ADDRESS)).to.be.true
           })
       })
   })
 
   after(function () {
-    return testHelper.deleteAll(reference, 'IntSchema')
+    return testHelper.deleteAll(REFERENCE, 'IntSchema')
   })
 })
