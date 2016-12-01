4aaf926df307ce38f24cdc6b5933ec1cfe3bf640
Gary Douglas <garyd@kainos.com>
2016-12-01 14:40:04 +0000

    Getting last manually approved claim in auto approval process should now compare by DateReviewed instead of DateOfJourney

--- app/services/data/get-data-for-auto-approval-check.js
+++ app/services/data/get-data-for-auto-approval-check.js
@@ -62,11 +62,20 @@ function getPreviousClaims (claimId, eligibilityId, dateOfVisit) {
 function getLatestManuallyApprovedClaim (previousClaims) {
   if (previousClaims.length > 0) {
     var result = {}
-    var latestManuallyApprovedClaim = previousClaims[0]
+    var latestManuallyApprovedClaim = null
 
     previousClaims.forEach(function (previousClaim) {
-      if (previousClaim.Status === statusEnum.APPROVED && previousClaim.DateSubmitted < latestManuallyApprovedClaim.DateSubmitted) {
-        latestManuallyApprovedClaim = previousClaim
+      var previousClaimIsApproved = previousClaim.DateReviewed &&
+        previousClaim.Status === statusEnum.APPROVED
+
+      if (previousClaimIsApproved) {
+        if (!latestManuallyApprovedClaim) {
+          latestManuallyApprovedClaim = previousClaim
+        } else {
+          if (previousClaim.DateReviewed < latestManuallyApprovedClaim.DateReviewed) {
+            latestManuallyApprovedClaim = previousClaim
+          }
+        }
       }
     })
 
