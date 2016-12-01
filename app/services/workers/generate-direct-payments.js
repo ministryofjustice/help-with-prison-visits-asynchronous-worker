fcec9d301aad09801c21bbfe3026a9e7bd53f0e3
gdouglas1 <garyd@kainos.com>
2016-12-01 12:43:51 +0000

    Updated claims payment processing logic to now record payment amount, and to update using claimIds instead of references (#50)

--- app/services/workers/generate-direct-payments.js
+++ app/services/workers/generate-direct-payments.js
@@ -6,21 +6,46 @@ const fileTypes = require('../../constants/payment-filetype-enum')
 const _ = require('lodash')
 
 module.exports.execute = function (task) {
-  var references
+  var claimIds
 
   return getClaimsPendingPayment()
     .then(function (paymentData) {
       if (paymentData.length > 0) {
-        var referenceIndex = 4
-        references = _.map(paymentData, p => { return p[referenceIndex] })
+        var claimIdIndex = 0
+        claimIds = getClaimIdsFromPaymentData(paymentData, claimIdIndex)
+        removeClaimIdsFromPaymentData(paymentData, claimIdIndex)
 
         return createPaymentFile(paymentData)
           .then(function (result) {
             return insertDirectPaymentFile(result, fileTypes.ACCESSPAY_FILE)
               .then(function () {
-                return updateClaimsProcessedPayment(references)
+                return updateAllClaimsProcessedPayment(claimIds, paymentData)
               })
           })
       }
     })
+}
+
+function getClaimIdsFromPaymentData (paymentData, claimIdIndex) {
+  return _.map(paymentData, p => { return p[claimIdIndex] })
+}
+
+function removeClaimIdsFromPaymentData (paymentData, claimIdIndex) {
+  paymentData.forEach(function (data) { data.splice(claimIdIndex, 1) })
+
+  return paymentData
+}
+
+function updateAllClaimsProcessedPayment (claimIds, paymentData) {
+  var promises = []
+
+  for (var i = 0; i < claimIds.length; i++) {
+    var claimPaymentData = paymentData[i]
+    var claimId = claimIds[i]
+
+    var totalApprovedCostIndex = 3
+    promises.push(updateClaimsProcessedPayment(claimId, parseFloat(claimPaymentData[totalApprovedCostIndex])))
+  }
+
+  return Promise.all(promises)
 }
