e20fc7d1c139bf3657ad07ed49b1a4a8232473aa
Gary Douglas <garyd@kainos.com>
2016-12-01 15:02:42 +0000

    Removed code to spawn multiple workers, removed async worker config options that are no longer used

--- start.js
+++ start.js
@@ -4,33 +4,23 @@ var CronJob = require('cron').CronJob
 var processTasks = require('./app/process-tasks')
 
 var asyncWorkerCron = config.ASYNC_WORKER_CRON
-var numberOfWorkers = config.ASYNC_WORKER_CONCURRENCY
-var startWeb = config.ASYNC_START_WEB === 'true'
 
-for (var i = 0; i < numberOfWorkers; i++) {
-  var workerId = i + 1
+var asyncWorkerJob = new CronJob({
+  cronTime: asyncWorkerCron,
+  onTick: function () {
+    runProcessTasks()
+  },
+  onComplete: function () {
+    log.info('worker completed running task')
+  },
+  start: false
+})
+log.info('Started worker')
+asyncWorkerJob.start()
 
-  var directPaymentJob = new CronJob({
-    cronTime: asyncWorkerCron,
-    onTick: function () {
-      runProcessTasks(workerId)
 
-      if (startWeb && workerId === 1) {
-        // Start web
-        require('./app/web/bin/www')
-      }
-    },
-    onComplete: function () {
-      log.info(`worker ${workerId} completed running task`)
-    },
-    start: false
-  })
-  log.info(`Started worker ${workerId}`)
-  directPaymentJob.start()
-}
-
-function runProcessTasks (id) {
+function runProcessTasks () {
   return processTasks().then(function () {
-    log.info(`worker ${id} completed running task`)
+    log.info(`worker completed running task`)
   })
 }
