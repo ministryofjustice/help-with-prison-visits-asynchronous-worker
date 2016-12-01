98494031d378c1fbc767c4b754f0a76647ed2ff5
Gary Douglas <garyd@kainos.com>
2016-12-01 11:18:46 +0000

    Updated default config to fire worker every 2 seconds

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   // Worker
   ASYNC_START_WEB: process.env.APVS_ASYNC_START_WEB || 'false',
-  ASYNC_WORKER_CRON: process.env.APVS_PAYMENT_GENERATION_CRON || '*/2 * * * * * *',
+  ASYNC_WORKER_CRON: process.env.APVS_PAYMENT_GENERATION_CRON || '* */2 * * * * *',
   ASYNC_WORKER_CONCURRENCY: process.env.APVS_ASYNC_WORKER_CONCURRENCY || 1,
   ASYNC_WORKER_BATCH_SIZE: process.env.APVS_ASYNC_WORKER_BATCH_SIZE || 5,
 
