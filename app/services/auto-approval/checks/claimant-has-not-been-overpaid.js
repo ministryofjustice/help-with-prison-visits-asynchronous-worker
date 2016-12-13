a052c70c3e5ca54975ba39a3642c41269990c519
gdouglas1 <garyd@kainos.com>
2016-12-13 11:43:14 +0000

    Feature/apvs 370/overpayments (#80)
    * Added auto approval check for overpaid claimants
    
    * Updated failure message on claimant-has-not-been-overpaid check
    
    * Only check for IsOverpaid, ignore IsAdvanceClaim
    
    * Fixed unit test

---
+++ app/services/auto-approval/checks/claimant-has-not-been-overpaid.js
@@ -0,0 +1,16 @@
+const AutoApprovalCheckResult = require('../../domain/auto-approval-check-result')
+
+const CHECK_NAME = 'claimant-has-not-been-overpaid'
+const FAILURE_MESSAGE = 'This claimant has been flagged as being overpaid on a previous claim'
+
+module.exports = function (autoApprovalData) {
+  var checkPassed = true
+
+  autoApprovalData.previousClaims.forEach(function (previousClaim) {
+    if (previousClaim.IsOverpaid === true) {
+      checkPassed = false
+    }
+  })
+
+  return new AutoApprovalCheckResult(CHECK_NAME, checkPassed, checkPassed ? '' : FAILURE_MESSAGE)
+}
