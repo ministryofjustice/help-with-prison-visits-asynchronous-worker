020f6177fdb7ede6a8abc41f471c9e749c00ebf9
Steven Alexander <steven.william.alexander@googlemail.com>
2016-12-14 09:21:06 +0000

    add requestInfoUrl data to request information notification

--- app/services/workers/send-request-information-claim-notification.js
+++ app/services/workers/send-request-information-claim-notification.js
@@ -3,7 +3,8 @@ const sendNotification = require('../notify/send-notification')
 
 module.exports.execute = function (task) {
   var reference = task.reference
-  var personalisation = {reference: reference} // TODO information breakdown
+  var requestInfoUrl = `${config.EXTERNAL_SERVICE_URL}${config.EXTERNAL_PATH_ALREADY_REGISTERED}`
+  var personalisation = {reference: reference, requestInfoUrl: requestInfoUrl}
 
   var emailAddress = task.additionalData
   var emailTemplateId = config.NOTIFY_REQUEST_INFORMATION_CLAIM_EMAIL_TEMPLATE_ID
