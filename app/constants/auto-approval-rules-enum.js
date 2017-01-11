1f01197ad17c695b4b5df7dfaf60ddcb95b966cf
gdouglas1 <garyd@kainos.com>
2017-01-11 14:33:08 +0000

    Added missing checks to auto-approval-rules-enum (#106)

--- app/constants/auto-approval-rules-enum.js
+++ app/constants/auto-approval-rules-enum.js
@@ -1,16 +1,18 @@
 // NOTE: Keep in sync with apvs-internal-web to ensure all checks can be disabled in admin config
 module.exports = [
   'are-children-under-18',
+  'claimant-has-not-been-overpaid',
   'cost-and-variance-equal-or-less-than-first-time-claim',
   'do-expenses-match-first-time-claim',
   'has-claimed-less-than-max-times-this-year',
+  'has-claimed-less-than-max-times-this-month',
   'has-uploaded-prison-visit-confirmation-and-receipts',
   'is-claim-submitted-within-time-limit',
   'is-claim-total-under-limit',
+  'is-claimant-trusted',
   'is-latest-manual-claim-approved',
   'is-no-previous-pending-claim',
   'is-prison-not-in-guernsey-jersey',
   'is-visit-in-past',
-  'visit-date-different-to-previous-claims',
-  'is-claimant-trusted'
+  'visit-date-different-to-previous-claims'
 ]
