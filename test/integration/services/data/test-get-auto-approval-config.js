d3da965834aaac91ac86cb5cc63a4a9a46419314
Gary Douglas <garyd@kainos.com>
2017-01-16 14:07:43 +0000

    Updated test to ensure default values are retrieved when required

--- test/integration/services/data/test-get-auto-approval-config.js
+++ test/integration/services/data/test-get-auto-approval-config.js
@@ -1,44 +1,33 @@
 var expect = require('chai').expect
 var getAutoApprovalConfig = require('../../../../app/services/data/get-auto-approval-config')
-var config = require('../../../../knexfile').asyncworker
-var knex = require('knex')(config)
+var knexConfig = require('../../../../knexfile').asyncworker
+var knex = require('knex')(knexConfig)
+var config = require('../../../../config')
 var moment = require('moment')
 
 describe('services/data/get-auto-approval-config', function () {
   var insertedIds
 
-  before(function () {
-    return knex('IntSchema.AutoApprovalConfig')
-      .insert([{
-        Caseworker: 'caseworker1@test.com',
-        DateCreated: moment().toDate(),
-        AutoApprovalEnabled: 'true',
-        CostVariancePercentage: '5.00',
-        MaxClaimTotal: '100.00',
-        MaxDaysAfterAPVUVisit: '28',
-        MaxNumberOfClaimsPerYear: '10',
-        RulesDisabled: 'auto-approval-rule-1,auto-approval-rule-2,auto-approval-rule-3',
-        IsEnabled: 'true'
-      },
-      {
-        Caseworker: 'caseworker2@test.com',
-        DateCreated: moment().subtract(1, 'day').toDate(),
-        AutoApprovalEnabled: 'true',
-        CostVariancePercentage: '5.00',
-        MaxClaimTotal: '100.00',
-        MaxDaysAfterAPVUVisit: '28',
-        MaxNumberOfClaimsPerYear: '10',
-        RulesDisabled: 'auto-approval-rule-1,auto-approval-rule-3',
-        IsEnabled: 'false'
-      }])
-      .returning('AutoApprovalConfigId')
+  it('should return auto approval config defaults when no auto approval config data is found', function () {
+    return getAutoApprovalConfig()
       .then(function (result) {
-        insertedIds = result
+        expect(result.AutoApprovalEnabled).to.equal(config.AUTO_APPROVAL_ENABLED === 'true')
+        expect(result.CostVariancePercentage).to.equal(parseFloat(config.AUTO_APPROVAL_COST_VARIANCE))
+        expect(result.MaxClaimTotal).to.equal(parseFloat(config.AUTO_APPROVAL_MAX_CLAIM_TOTAL))
+        expect(result.MaxDaysAfterAPVUVisit).to.equal(parseInt(config.AUTO_APPROVAL_MAX_DAYS_AFTER_APVU_VISIT))
+        expect(result.MaxNumberOfClaimsPerYear).to.equal(parseInt(config.AUTO_APPROVAL_MAX_CLAIMS_PER_YEAR))
+        expect(result.MaxNumberOfClaimsPerMonth).to.equal(parseInt(config.AUTO_APPROVAL_MAX_CLAIMS_PER_MONTH))
+        expect(result.RulesDisabled).to.be.null
       })
   })
 
   it('should return the latest auto approval config record', function () {
-    return getAutoApprovalConfig()
+    return insertTestData()
+      .then(function (result) {
+        insertedIds = result
+
+        return getAutoApprovalConfig()
+      })
       .then(function (result) {
         expect(result.Caseworker).to.equal('caseworker1@test.com')
         expect(result.RulesDisabled).to.deep.equal(['auto-approval-rule-1', 'auto-approval-rule-2', 'auto-approval-rule-3'])
@@ -52,3 +41,30 @@ describe('services/data/get-auto-approval-config', function () {
       .del()
   })
 })
+
+function insertTestData () {
+  return knex('IntSchema.AutoApprovalConfig')
+    .insert([{
+      Caseworker: 'caseworker1@test.com',
+      DateCreated: moment().toDate(),
+      AutoApprovalEnabled: 'true',
+      CostVariancePercentage: '5.00',
+      MaxClaimTotal: '100.00',
+      MaxDaysAfterAPVUVisit: '28',
+      MaxNumberOfClaimsPerYear: '10',
+      RulesDisabled: 'auto-approval-rule-1,auto-approval-rule-2,auto-approval-rule-3',
+      IsEnabled: 'true'
+    },
+    {
+      Caseworker: 'caseworker2@test.com',
+      DateCreated: moment().subtract(1, 'day').toDate(),
+      AutoApprovalEnabled: 'true',
+      CostVariancePercentage: '5.00',
+      MaxClaimTotal: '100.00',
+      MaxDaysAfterAPVUVisit: '28',
+      MaxNumberOfClaimsPerYear: '10',
+      RulesDisabled: 'auto-approval-rule-1,auto-approval-rule-3',
+      IsEnabled: 'false'
+    }])
+    .returning('AutoApprovalConfigId')
+}
