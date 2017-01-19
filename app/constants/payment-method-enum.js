0e770203d5549b29de3a83d4597a156a24f6d465
Gary Douglas <garyd@kainos.com>
2017-01-19 12:01:46 +0000

    Updated payment-method-enum with manually processed

--- app/constants/payment-method-enum.js
+++ app/constants/payment-method-enum.js
@@ -2,5 +2,9 @@ module.exports = {
   DIRECT_BANK_PAYMENT: {
     value: 'bank',
     displayName: 'Direct Bank Payment'
+  },
+  MANUALLY_PROCESSED: {
+    value: 'manual',
+    displayName: 'Manually Processed Payment'
   }
 }
