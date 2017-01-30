bdc7cbb09fcd70c76a4dfc9bf98e0cf8340a2043
Haresh Vekriya <hareshv@nautilus.local>
2017-01-30 15:47:20 +0000

    updated content for submitted application email notification

--- test/unit/services/workers/test-send-claim-notification.js
+++ test/unit/services/workers/test-send-claim-notification.js
@@ -8,11 +8,16 @@ const config = require('../../../../config')
 var emailAddress = 'test@test.com'
 var reference = '1234567'
 var eligibilityId = '4321'
+var firstName = 'Joe'
 
 var stubSendNotification = sinon.stub().resolves()
+var stubGetFirstNameByReference = sinon.stub().resolves({
+  'FirstName': firstName
+})
 
 const sendClaimNotification = proxyquire('../../../../app/services/workers/send-claim-notification', {
-  '../notify/send-notification': stubSendNotification
+  '../notify/send-notification': stubSendNotification,
+  '../data/get-first-name-by-reference': stubGetFirstNameByReference
 })
 
 describe('services/send-claim-notification', function () {
@@ -27,6 +32,8 @@ describe('services/send-claim-notification', function () {
       expect(stubSendNotification.firstCall.args[0]).to.be.equal(config.NOTIFY_SUBMIT_CLAIM_EMAIL_TEMPLATE_ID)
       expect(stubSendNotification.firstCall.args[1]).to.be.equal(emailAddress)
       expect(stubSendNotification.firstCall.args[2].reference).to.be.equal(reference)
+      expect(stubSendNotification.firstCall.args[2].first_name).to.be.equal(firstName)
+      expect(stubSendNotification.firstCall.args[2].requestInfoUrl).not.to.be.null
     })
   })
 })
