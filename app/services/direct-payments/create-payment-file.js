ce00f0be4ebb28890c7890d8eebb8a6048f77681
gdouglas1 <garyd@kainos.com>
2017-01-30 16:29:00 +0000

    Include time in payment file name (#132)
    * Include time in payment file name
    
    * Re-run tests
    
    * Revert previous commit
    
    * Fixed hour format to 24hr

--- app/services/direct-payments/create-payment-file.js
+++ app/services/direct-payments/create-payment-file.js
@@ -29,6 +29,6 @@ function mkdirIfNotExists (dir) {
 }
 
 function getFileName () {
-  const datestamp = dateFormatter.now().format('YYYYMMDD')
+  const datestamp = dateFormatter.now().format('YYYYMMDDHHmmss')
   return `apvs-payments-${datestamp}.csv`
 }
