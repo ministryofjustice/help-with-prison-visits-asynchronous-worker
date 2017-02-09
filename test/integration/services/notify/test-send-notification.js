df01c31be12a8288b3909350a9f52150fa2b37b3
Steven Alexander <steven.william.alexander@googlemail.com>
2017-02-09 15:15:03 +0000

    undo commit used to test notifications

--- test/integration/services/notify/test-send-notification.js
+++ test/integration/services/notify/test-send-notification.js
@@ -3,24 +3,18 @@ const sendNotification = require('../../../../app/services/notify/send-notificat
 
 describe('services/notify/send-notification', function () {
   it('should send an email', function () {
-    var requestInfoUrl = `${config.EXTERNAL_SERVICE_URL}${config.EXTERNAL_PATH_ALREADY_REGISTERED}`
-    var personalisation = {
-      first_name: 'Steven',
-      reference: 'REF1234',
-      requestInfoUrl: requestInfoUrl
-    }
-
-    var emailAddress = 's.alexander@kainos.com'
-    var emailTemplateId = config.NOTIFY_REJECTED_CLAIM_EMAIL_TEMPLATE_ID
-
-    return sendNotification(emailTemplateId, emailAddress, personalisation)
+    return sendNotification(
+      config.NOTIFY_SUBMIT_CLAIM_EMAIL_TEMPLATE_ID,
+      config.APVS_TEST_EMAIL_ADDRESS,
+      { reference: 'DONOTSEND' }
+    ) // manual check to see if email was sent
   })
 
-  // it('should not send an email to do not send email', function () {
-  //   return sendNotification(
-  //     config.NOTIFY_SUBMIT_CLAIM_EMAIL_TEMPLATE_ID,
-  //     config.NOTIFY_DO_NOT_SEND_EMAIL,
-  //     { reference: 'DONOTSEND' }
-  //   ) // manual check to see if email was not sent
-  // })
+  it('should not send an email to do not send email', function () {
+    return sendNotification(
+      config.NOTIFY_SUBMIT_CLAIM_EMAIL_TEMPLATE_ID,
+      config.NOTIFY_DO_NOT_SEND_EMAIL,
+      { reference: 'DONOTSEND' }
+    ) // manual check to see if email was not sent
+  })
 })
