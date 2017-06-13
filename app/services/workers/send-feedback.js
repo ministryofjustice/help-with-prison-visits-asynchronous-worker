161906291cf751c09d39f9dbe2ec421042a40fb4
Nathanial Stewart <n.stewart@kainos.com>
2017-06-13 14:32:29 +0100

    Adding tags and adding submitter as helpwithprisonvisits agent account

--- app/services/workers/send-feedback.js
+++ app/services/workers/send-feedback.js
@@ -5,6 +5,7 @@ const Zendesk = require('zendesk-node-api')
 module.exports.execute = function (task) {
   var feedback = task.additionalData.split('~~')
   var subjectText = 'Help With Prison Visits - Feedback'
+  var tagText = ['HelpWithPrisonVisits']
   var personalisation = {
     rating: RatingEnum[feedback[0]].displayName,
     improvements: feedback[1],
@@ -20,21 +21,25 @@ module.exports.execute = function (task) {
 
     if (Config.ZENDESK_TEST_ENVIRONMENT === 'true') {
       subjectText = 'Test: Help With Prison Visits - Feedback'
+      tagText = ['HelpWithPrisonVisits', 'Test']
     }
 
     if (personalisation.contactEmailAddress === '') {
       return zendesk.tickets.create({
+        submitter_id: '114198238551',
         requester_id: '114198238551',
         subject: subjectText,
         comment: {
           body: personalisation.improvements + '\n\n' +
           'Rating: ' + personalisation.rating
-        }
+        },
+        tags: tagText
       }).then(function (result) {
         console.log('Zendesk ticket, ' + result.ticket.id + ' has been raised')
       })
     } else {
       return zendesk.tickets.create({
+        submitter_id: '114198238551',
         requester: {
           'name': personalisation.contactEmailAddress,
           'email': personalisation.contactEmailAddress,
@@ -44,7 +49,8 @@ module.exports.execute = function (task) {
         comment: {
           body: personalisation.improvements + '\n\n' +
           'Rating: ' + personalisation.rating
-        }
+        },
+        tags: tagText
       }).then(function (result) {
         console.log('Zendesk ticket, ' + result.ticket.id + ' has been raised')
       })
