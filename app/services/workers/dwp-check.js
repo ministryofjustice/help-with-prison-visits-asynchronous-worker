f89c9e4fe9dd91040fccfc378feb1bf75c983d09
Stephen Madden <stephenmad@kainos.com>
2017-10-03 18:07:11 +0100

    initial email on failed dwp check code

--- app/services/workers/dwp-check.js
+++ app/services/workers/dwp-check.js
@@ -2,12 +2,21 @@
 const getVisitorDwpBenefitCheckerData = require('../data/get-visitor-dwp-benefit-checker-data')
 const callDwpBenefitCheckerSoapService = require('../benefit-checker/call-dwp-benefit-checker-soap-service')
 const updateVisitorWithDwpBenefitCheckerResult = require('../data/update-visitor-with-dwp-benefit-checker-result')
+const insertTask = require('../data/insert-task')
+const tasksEnum = require('../../constants/tasks-enum')
 
 module.exports.execute = function (task) {
   return getVisitorDwpBenefitCheckerData(task.reference, task.eligibilityId, task.claimId)
     .then(function (visitorDwpBenefitCheckerData) {
       return callDwpBenefitCheckerSoapService(visitorDwpBenefitCheckerData)
         .then(function (benefitCheckerResult) {
+          if(benefitCheckerResult.result) {
+            insertTask(task.reference, task.eligibilityId, task.claimId, tasksEnum.DWP_FAILED_NOTIFICATION, benefitCheckerResult.result)
+          }
+
+          console.dir(task.reference + " " + task.eligibilityId + " " + task.claimId + " " + tasksEnum.DWP_FAILED_NOTIFICATION + " " + benefitCheckerResult.result)
+          console.log(task.reference + " " + task.eligibilityId + " " + task.claimId + " " + tasksEnum.DWP_FAILED_NOTIFICATION + " " + benefitCheckerResult.result)
+
           return updateVisitorWithDwpBenefitCheckerResult(benefitCheckerResult.visitorId, benefitCheckerResult.result)
         })
     })
---
+++ app/services/workers/send-claim-additional-information-required.js
@@ -0,0 +1,23 @@
+const config = require('../../../config')
+const sendNotification = require('../notify/send-notification')
+const getFirstNameByClaimId = require('../data/get-first-name-by-claimId')
+
+module.exports.execute = function (task) {
+  var claimId = task.claimId
+  var reference = task.reference
+
+  return getFirstNameByClaimId('IntSchema', claimId)
+  .then(function (firstName) {
+    var requestInfoUrl = `${config.EXTERNAL_SERVICE_URL}${config.EXTERNAL_PATH_ALREADY_REGISTERED}`
+    var personalisation = {
+      first_name: firstName,
+      reference: reference,
+      requestInfoUrl: requestInfoUrl
+    }
+
+    var emailAddress = task.additionalData
+    var emailTemplateId = config.NOTIFY_SUBMIT_CLAIM_EMAIL_TEMPLATE_ID
+
+    return sendNotification(emailTemplateId, emailAddress, personalisation)
+  })
+}
