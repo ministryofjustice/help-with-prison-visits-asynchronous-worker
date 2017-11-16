334f3af9ba7c3b4b04a7f88a9012f45cc83f2686
Stephen Madden <stephenmad@kainos.com>
2017-11-16 14:56:04 +0000

    Updated to only send DWP failed email for certain benefit types

--- app/services/data/get-visitor-dwp-benefit-checker-data.js
+++ app/services/data/get-visitor-dwp-benefit-checker-data.js
@@ -7,13 +7,14 @@ module.exports = function (reference, eligibilityId, claimId) {
     .join('IntSchema.Claim', 'IntSchema.Eligibility.EligibilityId', '=', 'IntSchema.Claim.EligibilityId')
     .join('IntSchema.Visitor', 'IntSchema.Eligibility.EligibilityId', '=', 'IntSchema.Visitor.EligibilityId')
     .where({'IntSchema.Eligibility.Reference': reference, 'IntSchema.Eligibility.EligibilityId': eligibilityId, 'IntSchema.Claim.ClaimId': claimId})
-    .first('IntSchema.Visitor.VisitorId', 'IntSchema.Visitor.LastName', 'IntSchema.Visitor.NationalInsuranceNumber', 'IntSchema.Visitor.DateOfBirth')
+    .first('IntSchema.Visitor.VisitorId', 'IntSchema.Visitor.LastName', 'IntSchema.Visitor.NationalInsuranceNumber', 'IntSchema.Visitor.DateOfBirth', 'IntSchema.Visitor.Benefit')
     .then(function (result) {
       return {
         'visitorId': result.VisitorId,
         'nino': result.NationalInsuranceNumber.trim().toUpperCase(),
         'surname': result.LastName.trim().toUpperCase(),
-        'dateOfBirth': moment(result.DateOfBirth).format('YYYYMMDD')
+        'dateOfBirth': moment(result.DateOfBirth).format('YYYYMMDD'),
+        'benefit': result.Benefit
       }
     })
 }
