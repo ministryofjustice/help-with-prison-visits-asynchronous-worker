db18cfddff7acaad9f7d0f462299f803e9a0e83a
Stephen Madden <stephenmad@kainos.com>
2017-11-17 11:26:13 +0000

    Updating claim status to request information

--- app/services/workers/dwp-check.js
+++ app/services/workers/dwp-check.js
@@ -4,8 +4,10 @@ const callDwpBenefitCheckerSoapService = require('../benefit-checker/call-dwp-be
 const updateVisitorWithDwpBenefitCheckerResult = require('../data/update-visitor-with-dwp-benefit-checker-result')
 const insertTask = require('../data/insert-task')
 const tasksEnum = require('../../constants/tasks-enum')
+const statusEnum = require('../../constants/status-enum')
 const claimEventEnum = require('../../constants/claim-event-enum')
 const insertClaimEvent = require('../data/insert-claim-event')
+const updateClaimStatus = require('../data/update-claim-status')
 const sendDWPFailedEmailEnum = require('../../constants/dwp-failed-email-enum')
 
 module.exports.execute = function (task) {
@@ -15,8 +17,9 @@ module.exports.execute = function (task) {
         .then(function (benefitCheckerResult) {
           if(benefitCheckerResult.result != "YES" && sendDWPFailedEmailEnum[visitorDwpBenefitCheckerData.benefit]) {
               return insertTask(task.reference, task.eligibilityId, task.claimId, tasksEnum.DWP_FAILED_NOTIFICATION, benefitCheckerResult.result)
-                .then(function() { return insertClaimEvent(task.reference, task.eligibilityId, task.claimId, null, claimEventEnum.MESSAGE.value, null, "Additional information automatically requested after DWP check", false) })
+                .then(function() { return insertClaimEvent(task.reference, task.eligibilityId, task.claimId, null, claimEventEnum.CLAIM_REQUEST_INFORMATION.value, null, "Additional information automatically requested after DWP check", false) })
                 .then(function() { return updateVisitorWithDwpBenefitCheckerResult(benefitCheckerResult.visitorId, benefitCheckerResult.result) })
+                .then(function() { return updateClaimStatus(task.claimId, statusEnum.REQUEST_INFORMATION) })
           } else {
             return updateVisitorWithDwpBenefitCheckerResult(benefitCheckerResult.visitorId, benefitCheckerResult.result)
           }
