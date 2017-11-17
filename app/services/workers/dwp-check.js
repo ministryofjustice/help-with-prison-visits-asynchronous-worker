ef9760977b24d0947aa70e65700b23ae7ebcec6c
Stephen Madden <stephenmad@kainos.com>
2017-11-17 14:55:00 +0000

    Correcting comparison operator

--- app/services/workers/dwp-check.js
+++ app/services/workers/dwp-check.js
@@ -15,7 +15,7 @@ module.exports.execute = function (task) {
     .then(function (visitorDwpBenefitCheckerData) {
       return callDwpBenefitCheckerSoapService(visitorDwpBenefitCheckerData)
         .then(function (benefitCheckerResult) {
-          if (benefitCheckerResult.result != 'YES' && sendDWPFailedEmailEnum[visitorDwpBenefitCheckerData.benefit]) {
+          if (benefitCheckerResult.result !== 'YES' && sendDWPFailedEmailEnum[visitorDwpBenefitCheckerData.benefit]) {
             return insertTask(task.reference, task.eligibilityId, task.claimId, tasksEnum.DWP_FAILED_NOTIFICATION, benefitCheckerResult.result)
                 .then(function () { return insertClaimEvent(task.reference, task.eligibilityId, task.claimId, null, claimEventEnum.CLAIM_REQUEST_INFORMATION.value, null, 'Additional information automatically requested after DWP check', false) })
                 .then(function () { return updateVisitorWithDwpBenefitCheckerResult(benefitCheckerResult.visitorId, benefitCheckerResult.result) })
