74d234415c39aab090a6d816b1b4642ebddd0e67
Stephen Madden <stephenmad@kainos.com>
2017-11-29 14:37:22 +0000

    Updating message added to claim when dwp email sent

--- app/services/workers/dwp-check.js
+++ app/services/workers/dwp-check.js
@@ -17,7 +17,7 @@ module.exports.execute = function (task) {
         .then(function (benefitCheckerResult) {
           if (benefitCheckerResult.result !== 'YES' && sendDWPFailedEmailEnum[visitorDwpBenefitCheckerData.benefit]) {
             return insertTask(task.reference, task.eligibilityId, task.claimId, tasksEnum.DWP_FAILED_NOTIFICATION, visitorDwpBenefitCheckerData.email)
-                .then(function () { return insertClaimEvent(task.reference, task.eligibilityId, task.claimId, null, claimEventEnum.CLAIM_REQUEST_INFORMATION.value, null, 'Additional information automatically requested after DWP check', true) })
+                .then(function () { return insertClaimEvent(task.reference, task.eligibilityId, task.claimId, null, claimEventEnum.CLAIM_REQUEST_INFORMATION.value, null, 'Automatic claim update:<br /><br />Requesting additional benefit information to be uploaded', false) })
                 .then(function () { return updateVisitorWithDwpBenefitCheckerResult(benefitCheckerResult.visitorId, benefitCheckerResult.result) })
                 .then(function () { return updateClaimStatus(task.claimId, statusEnum.REQUEST_INFORMATION) })
           } else {
