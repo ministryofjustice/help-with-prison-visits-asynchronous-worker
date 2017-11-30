4b4169f860684073f445526a07666c6128fd6486
Stephen Madden <stephenmad@kainos.com>
2017-11-30 16:26:12 +0000

    Adding dummy benefit document so the process works correctly down the line. Connected to visitor table to retrieve benefit type

--- app/services/workers/dwp-check.js
+++ app/services/workers/dwp-check.js
@@ -9,6 +9,9 @@ const claimEventEnum = require('../../constants/claim-event-enum')
 const insertClaimEvent = require('../data/insert-claim-event')
 const updateClaimStatus = require('../data/update-claim-status')
 const sendDWPFailedEmailEnum = require('../../constants/dwp-failed-email-enum')
+const config = require('../../../knexfile').asyncworker
+const knex = require('knex')(config)
+const log = require('../../services/log')
 
 module.exports.execute = function (task) {
   return getVisitorDwpBenefitCheckerData(task.reference, task.eligibilityId, task.claimId)
@@ -17,7 +20,8 @@ module.exports.execute = function (task) {
         .then(function (benefitCheckerResult) {
           if (benefitCheckerResult.result !== 'YES' && sendDWPFailedEmailEnum[visitorDwpBenefitCheckerData.benefit]) {
             return insertTask(task.reference, task.eligibilityId, task.claimId, tasksEnum.DWP_FAILED_NOTIFICATION, visitorDwpBenefitCheckerData.email)
-                .then(function () { return insertClaimEvent(task.reference, task.eligibilityId, task.claimId, null, claimEventEnum.CLAIM_REQUEST_INFORMATION.value, null, 'Automatic claim update:\n\nRequesting additional benefit information to be uploaded', false) })
+                .then(function () { return insertDummyUploadLaterBenefitDocument(task.claimId, task.eligibilityId, task.reference) })
+                .then(function () { return insertClaimEvent(task.reference, task.eligibilityId, task.claimId, null, claimEventEnum.CLAIM_REQUEST_INFORMATION.value, null, 'We have not been able to verify your benefit status. Please upload evidence of the benefit you receive', false) })
                 .then(function () { return updateVisitorWithDwpBenefitCheckerResult(benefitCheckerResult.visitorId, benefitCheckerResult.result, statusEnum.REQUEST_INFORMATION) })
                 .then(function () { return updateClaimStatus(task.claimId, statusEnum.REQUEST_INFORMATION) })
           } else {
@@ -25,4 +29,21 @@ module.exports.execute = function (task) {
           }
         })
     })
+}
+
+function insertDummyUploadLaterBenefitDocument(claimId, eligibilityId, reference) {
+  return knex('IntSchema.Visitor')
+  .where({'Reference': reference, 'EligibilityId': eligibilityId})
+  .first('benefit')
+  .then(function (result) {
+    return knex('IntSchema.ClaimDocument').insert({
+      ClaimDocumentId: (Math.floor(Date.now() / 100) - 14000000000) + 2, // taken from internal-claim-document.helper.js on external web
+      ClaimId: claimId,
+      EligibilityId: eligibilityId,
+      Reference: reference,
+      DocumentType: result.benefit,
+      DocumentStatus: 'upload-later',
+      IsEnabled: true
+    })
+  })
 }
