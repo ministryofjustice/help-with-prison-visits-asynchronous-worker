ea4f98acb59c2659b1974d0f7e917838d775693d
kevinfox <kevinfox@kainos.com>
2019-07-24 15:41:34 +0100

    APVS0177: Removing unnecessary then statement and log statements

--- app/services/workers/auto-approve-claims.js
+++ app/services/workers/auto-approve-claims.js
@@ -1,19 +1,16 @@
 const getAutoApproveClaims = require('../data/get-auto-approve-claim')
 const autoApproveClaim = require('../data/auto-approve-claim')
 const deleteAutoApproveClaim = require('../data/delete-auto-approve-claim')
-const log = require('../log')
 
 module.exports.execute = function (task) {
   var claimData
 
   return getAutoApproveClaims()
-    .then(function (data) { claimData = data })
-    .then(function () {
+    .then(function (data) {
+      claimData = data
       claimData.forEach(function (claim) {
-        log.info(claim)
         return autoApproveClaim(claim.Reference, claim.EligibilityId, claim.ClaimId, claim.EmailAddress)
           .then(function () {
-            log.info(claim.AutoApprovalId)
             return deleteAutoApproveClaim(claim.AutoApprovalId)
           })
       })
