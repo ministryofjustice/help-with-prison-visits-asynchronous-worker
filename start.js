8ee49709a978a1ea8b962c1c3f5f0ccecb3099d9
kevinfox <kevinfox@kainos.com>
2020-02-04 09:38:45 +0000

    APVS0213: Setting up app insights to send live metrics

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const appInsights = require('applicationinsights')
 const config = require('./config')
 appInsights.setup(config.APP_INSIGHTS_INSTRUMENTATION_KEY)
+.setSendLiveMetrics(true)
 appInsights.start()
 const log = require('./app/services/log')
 var CronJob = require('cron').CronJob
