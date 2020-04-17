04e4d97c84694aab50ebcd63e69f41af0a12e4e1
kevinfox <kevinfox@kainos.com>
2020-04-17 11:51:50 +0100

    APVS0199: Adding benefit expiry auto-approval check

--- app/services/auto-approval/checks/is-benefit-expiry-date-in-future.js
+++ app/services/auto-approval/checks/is-benefit-expiry-date-in-future.js
@@ -1,16 +1,23 @@
 const moment = require('moment')
-const dateFormatter = require('../../date-formatter')
 
 const AutoApprovalCheckResult = require('../../domain/auto-approval-check-result')
 
 const CHECK_NAME = 'is-benefit-expiry-date-in-future'
-const FAILURE_MESSAGE = 'The date of visit for this claim is not in the past at time of processing'
+const FAILURE_MESSAGE = 'The visit date is after the benefit expiry date'
 
 module.exports = function (autoApprovalData) {
-  var now = dateFormatter.now()
-  var dateOfVisit = moment(autoApprovalData.Visitor.BenefitExpiryDate)
+  var benefitExpiryDate = null
+  if (autoApprovalData.Visitor.BenefitExpiryDate) {
+    benefitExpiryDate = moment(autoApprovalData.Visitor.BenefitExpiryDate)
+  }
+  var dateOfJourney = moment(autoApprovalData.Claim.DateOfJourney)
 
-  var checkPassed = dateOfVisit.isAfter(now)
+  var checkPassed = false
+  if (benefitExpiryDate) {
+    checkPassed = benefitExpiryDate.isAfter(dateOfJourney)
+  } else {
+    return new AutoApprovalCheckResult(CHECK_NAME, checkPassed, checkPassed ? '' : 'The Benefit Expiry Date has not been set')
+  }
 
   return new AutoApprovalCheckResult(CHECK_NAME, checkPassed, checkPassed ? '' : FAILURE_MESSAGE)
 }
