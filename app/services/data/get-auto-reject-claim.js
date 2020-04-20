8c49e1c1841b1bf8e773e2a812617c5d6dee12fc
Nathanial Stewart <n.stewart@kainos.com>
2020-04-20 15:55:09 +0100

    Fixing errors

--- app/services/data/get-auto-reject-claim.js
+++ app/services/data/get-auto-reject-claim.js
@@ -3,8 +3,8 @@ const statusEnum = require('../../constants/status-enum')
 const knex = require('knex')(config)
 
 module.exports = function (olderThanDate) {
-	return knex('IntSchema.Claim')
-    .where('Claim.LastUpdated', '<', olderThanDate)
-    .andWhere('Claim.Status' = statusEnum.PENDING)
-    .select('Reference','EligibilityId','ClaimId')
+  return knex('IntSchema.Claim')
+    .where('LastUpdated', '<', olderThanDate)
+    .andWhere('Status', statusEnum.PENDING)
+    .select('Reference', 'EligibilityId', 'ClaimId')
 }
--- app/services/workers/auto-reject-claims.js
+++ app/services/workers/auto-reject-claims.js
@@ -1,9 +1,9 @@
-const getAutoApproveClaims = require('../data/get-auto-reject-claim')
+const getAutoRejectClaims = require('../data/get-auto-reject-claim')
 const insertClaimEventSystemMessage = require('../data/insert-claim-event-system-message')
 const updateClaimStatus = require('../data/update-claim-status')
-const deleteAutoApproveClaim = require('../data/delete-auto-approve-claim')
 const statusEnum = require('../../constants/status-enum')
 const claimEventEnum = require('../../constants/claim-event-enum')
+const dateFormatter = require('../date-formatter')
 const config = require('../../../config')
 
 module.exports.execute = function (task) {
@@ -11,13 +11,14 @@ module.exports.execute = function (task) {
   var autoRejectClaimsAfterWeeks = parseInt(config.AUTO_REJECT_AFTER_WEEKS)
   var autoRejectClaimsOlderThan = dateFormatter.now().subtract(autoRejectClaimsAfterWeeks, 'weeks').toDate()
 
-  return getAutoRejectClaims()
+  return getAutoRejectClaims(autoRejectClaimsOlderThan)
     .then(function (data) {
       claimData = data
       claimData.forEach(function (claim) {
-        return insertClaimEventSystemMessage(claim.Reference, claim.EligibilityId, claim.ClaimId, null, claimEventEnum.CLAIM_REJECTED.value, null, 'We have not had an update for ' + autoRejectClaimsAfterWeeks + ' weeks. Rejecting the claim.', false) })
+        return insertClaimEventSystemMessage(claim.Reference, claim.EligibilityId, claim.ClaimId, null, claimEventEnum.CLAIM_REJECTED.value, null, 'We have not had an update for ' + autoRejectClaimsAfterWeeks + ' weeks. Rejecting the claim.', false)
           .then(function () {
             return updateClaimStatus(claim.ClaimId, statusEnum.REJECTED)
           })
       })
+    })
 }
