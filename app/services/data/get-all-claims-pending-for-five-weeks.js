a69ad584cedf9dca410038ad0e9ba8e8ddc5f119
kevinfox <kevinfox@kainos.com>
2020-07-31 14:55:45 +0100

    APVS0181: Changing reminder email to 5 weeks of inactivity and rejection to 6 weeks

--- app/services/data/get-all-claims-pending-for-five-weeks.js
+++ app/services/data/get-all-claims-pending-for-five-weeks.js
@@ -4,12 +4,12 @@ const claimStatusEnum = require('../../constants/claim-status-enum')
 
 module.exports = function (reminderCutoffDate) {
   return knex('IntSchema.Claim')
-  .join('IntSchema.Visitor', 'IntSchema.Claim.EligibilityId', '=', 'IntSchema.Visitor.EligibilityId')
-  .where('Claim.LastUpdated', '<', reminderCutoffDate)
-  .where(function () {
-    this.where('IntSchema.Claim.ReminderSent', false)
-      .orWhereNull('IntSchema.Claim.ReminderSent')
-  })
-  .whereIn('Claim.Status', [claimStatusEnum.PENDING, claimStatusEnum.REQUEST_INFORMATION, claimStatusEnum.REQUEST_INFO_PAYMENT])
-  .select('IntSchema.Claim.EligibilityId', 'IntSchema.Claim.Reference', 'IntSchema.Claim.ClaimId', 'IntSchema.Visitor.EmailAddress')
+    .join('IntSchema.Visitor', 'IntSchema.Claim.EligibilityId', '=', 'IntSchema.Visitor.EligibilityId')
+    .where('Claim.LastUpdated', '<', reminderCutoffDate)
+    .where(function () {
+      this.where('IntSchema.Claim.ReminderSent', false)
+        .orWhereNull('IntSchema.Claim.ReminderSent')
+    })
+    .whereIn('Claim.Status', [claimStatusEnum.PENDING, claimStatusEnum.REQUEST_INFORMATION, claimStatusEnum.REQUEST_INFO_PAYMENT])
+    .select('IntSchema.Claim.EligibilityId', 'IntSchema.Claim.Reference', 'IntSchema.Claim.ClaimId', 'IntSchema.Visitor.EmailAddress')
 }
--- app/services/data/get-auto-reject-claim.js
+++ app/services/data/get-auto-reject-claim.js
@@ -6,7 +6,7 @@ module.exports = function (rejectionCutoffDate) {
   return knex('IntSchema.Claim')
     .join('IntSchema.Visitor', 'IntSchema.Claim.EligibilityId', '=', 'IntSchema.Visitor.EligibilityId')
     .where('Claim.DateReminderSent', '<', rejectionCutoffDate)
-    .where('Claim.ReminderSent', true )
+    .where('Claim.ReminderSent', true)
     .whereIn('Claim.Status', [claimStatusEnum.PENDING, claimStatusEnum.REQUEST_INFORMATION, claimStatusEnum.REQUEST_INFO_PAYMENT])
     .select('IntSchema.Claim.EligibilityId', 'IntSchema.Claim.Reference', 'IntSchema.Claim.ClaimId', 'IntSchema.Visitor.EmailAddress')
 }
