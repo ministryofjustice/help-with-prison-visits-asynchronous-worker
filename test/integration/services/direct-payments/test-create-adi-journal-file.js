1d3c445e67cc837e104e194aff801c12ce59a468
kevinfox <kevinfox@kainos.com>
2020-10-28 11:20:30 +0000

    APVS0234/APVS0243: Fixing integration tests

--- test/integration/services/direct-payments/test-create-adi-journal-file.js
+++ test/integration/services/direct-payments/test-create-adi-journal-file.js
@@ -12,7 +12,7 @@ var testFilePath
 
 describe('services/direct-payments/create-adi-journal-file', function () {
   it('should generate ADI Journal file', function () {
-    var accountingDate = dateFormatter.now().format('DD MMM YYYY')
+    var accountingDate = dateFormatter.now().format('DD-MMM-YYYY')
     var amount = 123.45
     var journalName = config.ADI_JORNAL_PREFIX + dateFormatter.now().format('DDMMYY') + config.ADI_JORNAL_SUFFIX
     return createAdiJournalFile(amount)
@@ -27,11 +27,13 @@ describe('services/direct-payments/create-adi-journal-file', function () {
             var accountingDateCellValue = adiJournalSheet.cell(config.ADI_ACCOUNTING_DATE_CELL).value()
             var periodCellValue = adiJournalSheet.cell(config.ADI_PERIOD_CELL).value()
             var adiJournalNameCellValue = adiJournalSheet.cell(config.ADI_JORNAL_NAME_CELL).value()
+            var adiJournalDescriptionCellValue = adiJournalSheet.cell(config.ADI_JORNAL_DESCRIPTION_CELL).value()
             expect(totalCellValue, 'Total Cell should have the value ' + amount).to.be.eql(amount)
             expect(debitCellValue, 'Debit Cell should have the value ' + amount).to.be.eql(amount)
             expect(accountingDateCellValue, 'Accounting Date Cell should have the value ' + accountingDate).to.be.eql(accountingDate)
             expect(periodCellValue, 'Period cell should be blank').to.be.eql(undefined)
-            expect(adiJournalNameCellValue, 'Jornal cell name cell should have the value ' + journalName).to.be.eql(journalName)
+            expect(adiJournalNameCellValue, 'Journal name cell should have the value ' + journalName).to.be.eql(journalName)
+            expect(adiJournalDescriptionCellValue, 'Journal description cell should have the value ' + journalName).to.be.eql(journalName)
           })
           .catch(function (error) {
             log.error('An error occurred while reading the test journal ')
