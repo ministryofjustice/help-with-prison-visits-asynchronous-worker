a2cc58197d4d687924cd45a2da9e5e591feb0fdf
kevinfox <kevinfox@kainos.com>
2020-11-24 12:18:43 +0000

    APVS0144: Stopping logs from being sent to a now defunct server. Resolving Error: connect ECONNREFUSED

--- app/services/log.js
+++ app/services/log.js
@@ -1,12 +1,9 @@
 const config = require('../../config')
 const bunyan = require('bunyan')
-const bunyanLogstash = require('bunyan-logstash-tcp')
 const PrettyStream = require('bunyan-prettystream')
 
 const logsPath = config.LOGGING_PATH || 'logs/asynchronous-worker.log'
 const logsLevel = config.LOGGING_LEVEL
-const logstashHost = config.LOGSTASH_HOST
-const logstashPort = config.LOGSTASH_PORT
 
 // Stream to handle pretty printing of Bunyan logs to stdout.
 var prettyStream = new PrettyStream()
@@ -20,22 +17,6 @@ var log = bunyan.createLogger({
     error: errorSerializer
   }
 })
-
-// Add stream to push logs to Logstash for aggregation, reattempt connections indefinitely.
-if (logstashHost && logstashPort) {
-  var logstashStream = bunyanLogstash.createStream({
-    host: logstashHost,
-    port: logstashPort,
-    max_connect_retries: 10,
-    retry_interval: 1000 * 60
-  }).on('error', console.log)
-
-  log.addStream({
-    type: 'raw',
-    level: logsLevel,
-    stream: logstashStream
-  })
-}
 
 // Add console Stream.
 log.addStream({
