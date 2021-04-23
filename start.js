c61ce1ca07ab91aeec26f19bd8857071254956ef
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2021-04-23 11:20:50 +0100

    remove some semis

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const appInsights = require('applicationinsights')
 const config = require('./config')
 appInsights.setup(config.APP_INSIGHTS_INSTRUMENTATION_KEY)
