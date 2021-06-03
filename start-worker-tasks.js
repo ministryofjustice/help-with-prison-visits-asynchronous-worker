b38d4d5f28479475dcf7122080a3c86504018039
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2021-06-03 18:17:42 +0100

    Initial removal of npm cron

---
+++ start-worker-tasks.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+require('./app/azure-appinsights')
+const log = require('./app/services/log')
+const processTasks = require('./app/process-tasks')
+
+log.info('Started worker')
+
+processTasks().then(function () {
+  log.info('worker completed running task')
+})
