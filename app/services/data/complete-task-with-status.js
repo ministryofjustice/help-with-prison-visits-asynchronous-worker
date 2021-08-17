d67969a7a5bf84aa9006bbab02243a79858c1363
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2021-08-17 09:53:39 +0100

    Add more logging (#309)

--- app/services/data/complete-task-with-status.js
+++ app/services/data/complete-task-with-status.js
@@ -1,11 +1,17 @@
 const config = require('../../../knexfile').asyncworker
 const knex = require('knex')(config)
 const dateFormatter = require('../date-formatter')
+const log = require('../log')
 
 module.exports = function (schema, taskId, status) {
   return knex(`${schema}.Task`).where('TaskId', taskId)
     .update({
       Status: status,
       DateProcessed: dateFormatter.now().toDate()
+    })
+    .catch(function (error) {
+      log.error(`Failed to complete task with status: ${status}`)
+      log.error(error)
+      throw new Error(error)
     })
 }
--- app/services/data/get-pending-tasks-and-mark-inprogress.js
+++ app/services/data/get-pending-tasks-and-mark-inprogress.js
@@ -2,6 +2,7 @@ const config = require('../../../knexfile').asyncworker
 const knex = require('knex')(config)
 const statusEnum = require('../../constants/status-enum')
 const Task = require('../domain/task')
+const log = require('../log')
 
 module.exports = function (schema, batchSize) {
   return knex.select().table(`${schema}.Task`)
@@ -12,6 +13,7 @@ module.exports = function (schema, batchSize) {
       const tasks = []
       const ids = []
       if (results) {
+        log.info(`Found pending tasks for ${schema}`)
         for (const result of results) {
           ids.push(result.TaskId)
           tasks.push(new Task(
@@ -27,12 +29,22 @@ module.exports = function (schema, batchSize) {
             statusEnum.INPROGRESS))
         }
       } else {
+        log.info(`Found no pending tasks for ${schema}`)
         return []
       }
+
+      log.info(`Updating pending tasks status for ${schema}`)
       return knex(`${schema}.Task`).whereIn('TaskId', ids)
         .update('Status', statusEnum.INPROGRESS)
         .then(function () {
+          log.info(`Pending tasks status updated for ${schema}`)
           return tasks
+        }).catch(function (error) {
+          log.error(error)
+          return []
         })
+    }).catch(function (error) {
+      log.error(error)
+      return []
     })
 }
