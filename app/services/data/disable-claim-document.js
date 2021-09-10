4bb2ef7f829f3ff77dbc69cf0713dd49d9dae0eb
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2021-09-10 16:08:45 +0100

    Add more logging to debug request-information-response failures (#325)

--- app/services/data/disable-claim-document.js
+++ app/services/data/disable-claim-document.js
@@ -1,6 +1,8 @@
 const config = require('../../../knexfile').asyncworker
 const knex = require('knex')(config)
+const log = require('../log')
 
 module.exports = function (schema, claimDocumentId) {
+  log.info(`disableClaimDocument (${claimDocumentId})`)
   return knex(`${schema}.ClaimDocument`).update('IsEnabled', false).where('ClaimDocumentId', claimDocumentId)
 }
