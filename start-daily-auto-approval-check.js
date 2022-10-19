e22510a0d1cb6baf68f5c2843cc3db1e7de2f7f9
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2022-10-19 07:51:14 +0100

    Flush Application Insights data before exit

--- start-daily-auto-approval-check.js
+++ start-daily-auto-approval-check.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-require('./app/azure-appinsights')
+const appInsights = require('./app/azure-appinsights')
 const log = require('./app/services/log')
 const { autoApproveClaims } = require('./app/services/workers/auto-approve-claims')
 
@@ -8,9 +8,16 @@ log.info('Starting auto approval checks')
 autoApproveClaims()
   .then(function () {
     log.info('Auto approval checks completed')
-    process.exit()
+    if (appInsights) {
+      appInsights.flush({ callback: () => process.exit() })
+    }
   })
   .catch(function (error) {
     log.error('Failed auto approval checks', error)
+    if (appInsights) {
+      appInsights.flush({ callback: () => process.exit(1) })
+    }
+  })
+  .finally(() => {
     process.exit()
   })
