4e0826ba9a4e1726df8a01d0efbde22b15cef327
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-10-19 08:00:50 +0100

    VB-1319: Add logging and remove cached connection

--- app/databaseConnector.js
+++ app/databaseConnector.js
@@ -1,17 +1,20 @@
 require('dotenv').config()
 const knex = require('knex')
 const { KNEX_CONFIG } = require('../config')
+const log = require('./services/log')
 
-let cachedConnection
+// let cachedConnection
 
 function getDatabaseConnector (connectionDetails = KNEX_CONFIG) {
+  log.info('Getting database connection')
   const knexConfig = require('../knexfile')[connectionDetails]
 
-  if (cachedConnection) {
-    return cachedConnection
-  }
+  // if (cachedConnection) {
+  //   log.info('Returning cached ')
+  //   return cachedConnection
+  // }
   const connection = knex(knexConfig)
-  cachedConnection = connection
+  // cachedConnection = connection
   return connection
 }
 
