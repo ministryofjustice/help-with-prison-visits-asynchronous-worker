56e11b2b63782602c0b2c206a8092f6fb364b433
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-11-04 12:24:47 +0000

    Make appinsights nearer template

--- app/azure-appinsights.js
+++ app/azure-appinsights.js
@@ -1,6 +1,5 @@
 const appInsights = require('applicationinsights')
 const applicationVersion = require('./application-version')
-const ignoreNotFoundErrors = require('./telemetryProcessors/ignoreNotFound')
 
 const { packageData, buildNumber } = applicationVersion
 if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
@@ -10,10 +9,9 @@ if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
     .setup()
     .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
     .start()
-  module.exports = appInsights.defaultClient
   appInsights.defaultClient.context.tags['ai.cloud.role'] = packageData.name
   appInsights.defaultClient.context.tags['ai.application.ver'] = buildNumber
-  appInsights.defaultClient.addTelemetryProcessor(ignoreNotFoundErrors)
+  module.exports = appInsights.defaultClient
 } else {
   module.exports = null
 }
