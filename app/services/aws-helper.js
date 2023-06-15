1c6bd31ea874e70af9c567004e4c060c60256635
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-06-15 11:26:56 +0100

    HWPV-104: Move to IRSA S3 creds

--- app/services/aws-helper.js
+++ app/services/aws-helper.js
@@ -5,16 +5,9 @@ const log = require('./log')
 const config = require('../../config')
 
 class AWSHelper {
-  constructor ({ accessKeyId = config.AWS_ACCESS_KEY_ID, secretAccessKey = config.AWS_SECRET_ACCESS_KEY, bucketName = config.AWS_S3_BUCKET_NAME, region = config.AWS_REGION } = { }) {
-    this.accessKeyId = accessKeyId
-    this.secretAccessKey = secretAccessKey
+  constructor ({ bucketName = config.AWS_S3_BUCKET_NAME } = { }) {
     this.bucketName = bucketName
-    this.region = region
-    this.s3 = new S3({
-      accessKeyId: this.accessKeyId,
-      secretAccessKey: this.secretAccessKey,
-      region: this.region
-    })
+    this.s3 = new S3({})
   }
 
   async delete (key) {
