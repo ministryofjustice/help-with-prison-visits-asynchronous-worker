4208e5d5206f12eee888f585e008cf8f4709a242
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2023-06-21 11:42:02 +0100

    Remove overused db connection log messages (#412)
    * Remove overused db connection log messages
    
    * Remove unused include

--- app/databaseConnector.js
+++ app/databaseConnector.js
@@ -1,14 +1,10 @@
 require('dotenv').config()
 const knex = require('knex')
 const { KNEX_CONFIG } = require('../config')
-const log = require('./services/log')
 
 function getDatabaseConnector (connectionDetails = KNEX_CONFIG) {
-  log.info('Getting database connection')
   const knexConfig = require('../knexfile')[connectionDetails]
   const connection = knex(knexConfig)
-
-  log.info('Successfully got database connection')
 
   return connection
 }
