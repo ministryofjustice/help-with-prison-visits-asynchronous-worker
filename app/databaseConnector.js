f6dec958e514dfe17d114bea25904069637f2d7d
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-06-21 11:40:08 +0100

    Remove unused include

--- app/databaseConnector.js
+++ app/databaseConnector.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const knex = require('knex')
 const { KNEX_CONFIG } = require('../config')
-const log = require('./services/log')
 
 function getDatabaseConnector (connectionDetails = KNEX_CONFIG) {
   const knexConfig = require('../knexfile')[connectionDetails]
