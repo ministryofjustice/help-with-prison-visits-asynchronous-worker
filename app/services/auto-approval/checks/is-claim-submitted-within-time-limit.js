1972691c8712326c3220e19f1e5ba023ecdd1406
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2024-05-28 15:04:00 +0100

    HWPV-177: Make isClaimSubmittedWithinTimeLimit return true

--- app/services/auto-approval/checks/is-claim-submitted-within-time-limit.js
+++ app/services/auto-approval/checks/is-claim-submitted-within-time-limit.js
@@ -1,24 +1,24 @@
-const moment = require('moment')
-
+// const moment = require('moment')
 const AutoApprovalCheckResult = require('../../domain/auto-approval-check-result')
 
 const CHECK_NAME = 'is-claim-submitted-within-time-limit'
-const FAILURE_MESSAGE = 'Claim was not submitted with the time limit'
+// const FAILURE_MESSAGE = 'Claim was not submitted with the time limit'
 
-module.exports = function (autoApprovalData) {
-  const claimSubmissionDateMoment = moment(autoApprovalData.Claim.DateSubmitted)
-  let claimSubmissionCutOffDate
-  if (autoApprovalData.latestManuallyApprovedClaim) {
-    claimSubmissionCutOffDate = moment(autoApprovalData.latestManuallyApprovedClaim.DateReviewed).add(
-      autoApprovalData.maxDaysAfterAPVUVisit,
-      'days',
-    )
-  } else {
-    return new AutoApprovalCheckResult(CHECK_NAME, false, 'There is no manually approved claim for this eligibility')
-  }
+module.exports = function (_autoApprovalData) {
+  return new AutoApprovalCheckResult(CHECK_NAME, true, '')
+  // const claimSubmissionDateMoment = moment(_autoApprovalData.Claim.DateSubmitted)
+  // let claimSubmissionCutOffDate
+  // if (_autoApprovalData.latestManuallyApprovedClaim) {
+  //   claimSubmissionCutOffDate = moment(_autoApprovalData.latestManuallyApprovedClaim.DateReviewed).add(
+  //     _autoApprovalData.maxDaysAfterAPVUVisit,
+  //     'days',
+  //   )
+  // } else {
+  //   return new AutoApprovalCheckResult(CHECK_NAME, false, 'There is no manually approved claim for this eligibility')
+  // }
 
-  const checkPassed = claimSubmissionDateMoment.isSameOrBefore(claimSubmissionCutOffDate)
+  // const checkPassed = claimSubmissionDateMoment.isSameOrBefore(claimSubmissionCutOffDate)
 
-  const ADDITIONAL_INFO = `. Claim ref: ${autoApprovalData.Claim.Reference}, Claim submission date: ${claimSubmissionDateMoment.format('DD/MM/YYYY')}, Claim submission cut off date: ${claimSubmissionCutOffDate.format('DD/MM/YYYY')}`
-  return new AutoApprovalCheckResult(CHECK_NAME, checkPassed, checkPassed ? '' : FAILURE_MESSAGE + ADDITIONAL_INFO)
+  // const ADDITIONAL_INFO = `. Claim ref: ${_autoApprovalData.Claim.Reference}, Claim submission date: ${claimSubmissionDateMoment.format('DD/MM/YYYY')}, Claim submission cut off date: ${claimSubmissionCutOffDate.format('DD/MM/YYYY')}`
+  // return new AutoApprovalCheckResult(CHECK_NAME, checkPassed, checkPassed ? '' : FAILURE_MESSAGE + ADDITIONAL_INFO)
 }
