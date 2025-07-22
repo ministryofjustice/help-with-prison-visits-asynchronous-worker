0d0e5f34998d99be6a6580970ece6ec6e6c8b1ed
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2025-07-22 14:38:38 +0100

    Cause failed job to cause pod error (#492)

--- start-daily-auto-approval-check.js
+++ start-daily-auto-approval-check.js
@@ -11,15 +11,16 @@ setTimeout(() => {
       log.info('Auto approval checks completed')
       if (appInsights) {
         appInsights.flush({ callback: () => process.exit() })
+      } else {
+        process.exit()
       }
     })
     .catch(error => {
       log.error('Failed auto approval checks', error)
       if (appInsights) {
         appInsights.flush({ callback: () => process.exit(1) })
+      } else {
+        process.exit(1)
       }
-    })
-    .finally(() => {
-      process.exit()
     })
 }, 5000)
