34f71f4c7054fc00957625206ba9a144a6d11b58
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-10-20 09:10:21 +0100

    HWPV-271: Reduce auto approval calls

--- app/services/data/get-auto-approve-claim.js
+++ app/services/data/get-auto-approve-claim.js
@@ -1,13 +1,11 @@
 /* eslint-disable import/order */
 const config = require('../../../knexfile').asyncworker
 const knex = require('knex')(config)
+const statusEnum = require('../../constants/status-enum')
 
 module.exports = () => {
-  return knex('IntSchema.AutoApproval').select(
-    'AutoApprovalId',
-    'EligibilityId',
-    'Reference',
-    'ClaimId',
-    'EmailAddress',
-  )
+  return knex('IntSchema.AutoApproval')
+    .select('AutoApprovalId', 'EligibilityId', 'Reference', 'ClaimId', 'EmailAddress')
+    .leftJoin('IntSchema.Claim', 'IntSchema.Claim.ClaimId', '=', 'IntSchema.AutoApproval.ClaimId')
+    .whereNotIn('IntSchema.Claim.Status', [statusEnum.APPROVED, statusEnum.AUTOAPPROVED, statusEnum.REJECTED])
 }
