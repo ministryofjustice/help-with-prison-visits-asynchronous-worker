66be19f4dce4bca9e4e685ee76c545dcc7848992
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-12-18 14:17:47 +0000

    Choose random number that doesn't already exist

--- app/services/workers/helpers/insert-dummy-upload-later-benefit-document.js
+++ app/services/workers/helpers/insert-dummy-upload-later-benefit-document.js
@@ -4,14 +4,34 @@ const dateFormatter = require('../../date-formatter')
 module.exports = (claimId, benefit, eligibilityId, reference) => {
   const db = getDatabaseConnector()
 
-  return db('IntSchema.ClaimDocument').insert({
-    ClaimDocumentId: Math.floor(Math.random() * 4294967295), // Unsigned INT max value
-    ClaimId: claimId,
-    EligibilityId: eligibilityId,
-    Reference: reference,
-    DocumentType: benefit,
-    DocumentStatus: 'upload-later',
-    IsEnabled: true,
-    DateSubmitted: dateFormatter.now().toDate(),
-  })
+  return db('IntSchema.ClaimDocument')
+    .select('IntSchema.ClaimDocument.ClaimDocumentId')
+    .then(claimDocumentIds => {
+      return db('IntSchema.ClaimDocument').insert({
+        ClaimDocumentId: getRandomNumberExcludingList(claimDocumentIds),
+        ClaimId: claimId,
+        EligibilityId: eligibilityId,
+        Reference: reference,
+        DocumentType: benefit,
+        DocumentStatus: 'upload-later',
+        IsEnabled: true,
+        DateSubmitted: dateFormatter.now().toDate(),
+      })
+    })
+}
+
+function getRandomNumberExcludingList(list) {
+  const existingNumbers = list.map(id => id.ClaimDocumentId)
+
+  let randomNumber = getRandomUnsignedInt()
+
+  while (existingNumbers.includes(randomNumber)) {
+    randomNumber = getRandomUnsignedInt()
+  }
+
+  return randomNumber
+}
+
+function getRandomUnsignedInt() {
+  return Math.floor(Math.random() * 4294967295) // Unsigned INT max value
 }
