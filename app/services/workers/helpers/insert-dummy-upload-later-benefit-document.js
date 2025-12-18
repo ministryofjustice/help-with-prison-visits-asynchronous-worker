f4c7a61c4c80c61a475c31ab359f782dfaf70493
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-12-18 15:37:24 +0000

    Revert to original code

--- app/services/workers/helpers/insert-dummy-upload-later-benefit-document.js
+++ app/services/workers/helpers/insert-dummy-upload-later-benefit-document.js
@@ -4,34 +4,14 @@ const dateFormatter = require('../../date-formatter')
 module.exports = (claimId, benefit, eligibilityId, reference) => {
   const db = getDatabaseConnector()
 
-  return db('IntSchema.ClaimDocument')
-    .select('IntSchema.ClaimDocument.ClaimDocumentId')
-    .then(claimDocumentIds => {
-      return db('IntSchema.ClaimDocument').insert({
-        ClaimDocumentId: getRandomNumberExcludingList(claimDocumentIds),
-        ClaimId: claimId,
-        EligibilityId: eligibilityId,
-        Reference: reference,
-        DocumentType: benefit,
-        DocumentStatus: 'upload-later',
-        IsEnabled: true,
-        DateSubmitted: dateFormatter.now().toDate(),
-      })
-    })
-}
-
-function getRandomNumberExcludingList(list) {
-  const existingNumbers = list.map(id => id.ClaimDocumentId)
-
-  let randomNumber = getRandomUnsignedInt()
-
-  while (existingNumbers.includes(randomNumber)) {
-    randomNumber = getRandomUnsignedInt()
-  }
-
-  return randomNumber
-}
-
-function getRandomUnsignedInt() {
-  return Math.floor(Math.random() * 4294967295) // Unsigned INT max value
+  return db('IntSchema.ClaimDocument').insert({
+    ClaimDocumentId: Math.floor(Date.now() / 100) - 15000000000 + 2, // taken from internal-claim-document.helper.js on external web
+    ClaimId: claimId,
+    EligibilityId: eligibilityId,
+    Reference: reference,
+    DocumentType: benefit,
+    DocumentStatus: 'upload-later',
+    IsEnabled: true,
+    DateSubmitted: dateFormatter.now().toDate(),
+  })
 }
