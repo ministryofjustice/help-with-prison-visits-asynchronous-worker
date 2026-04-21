3620fadf72604a80c97b7e3cafb20f008ef2ca31
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2026-04-21 19:02:00 +0100

    Fix allow scripts build (#584)

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,8 +2,8 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/fsevents@2.3.3': 'ALLOW',
+    'node_modules/dtrace-provider@0.8.8': 'FORBID',
+    'node_modules/fsevents@2.3.3': 'FORBID',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
 })
